614998132727a3aacdcae094203b91f4f1882c38
Jame Zhan <jame@orderly.network>
2023-10-27 14:27:36 +0800

    update bottom sheet close icon

--- packages/component/src/icon/icons/index.ts
+++ packages/component/src/icon/icons/index.ts
@@ -4,3 +4,4 @@ export { EyeOffIcon } from "./eyeOff";
 export { MoveDownIcon } from "./moveDown";
 export { InfoIcon } from "./info";
 export { SwitchIcon } from "./switch";
+export { CloseIcon } from "./close";
