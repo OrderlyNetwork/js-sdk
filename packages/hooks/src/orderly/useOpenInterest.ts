ec5aa3733bb51e1774f7b53b658c1443e1093630
Mark <mark@orderly.network>
2023-11-03 15:23:03 +0800

    v2 issue - data 的數字應該從 ws 取，並自動刷新
    https://wootraders.atlassian.net/browse/DEX-554

---
+++ packages/hooks/src/orderly/useOpenInterest.ts
@@ -0,0 +1,20 @@
+import { useWS } from "../useWS";
+import useSWRSubscription from "swr/subscription";
+
+export const useOpenInterest = (symbol: string) => {
+  const ws = useWS();
+  return useSWRSubscription(`${symbol}@openinterest`, (key, { next }) => {
+    const unsubscribe = ws.subscribe(`${symbol}@openinterest`, {
+      onMessage: (message: any) => {
+        //
+        next(null, message.openInterest);
+      },
+    });
+
+    return () => {
+      //unsubscribe
+
+      unsubscribe?.();
+    };
+  });
+};
