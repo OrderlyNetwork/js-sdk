52988404674c6966415c1042f42f4949771b5adc
Mark <mark@orderly.network>
2023-11-16 11:34:07 +0800

    fix svg is hidden bug

--- packages/component/src/icon/icons/arrow.tsx
+++ packages/component/src/icon/icons/arrow.tsx
@@ -3,21 +3,23 @@ import { SVGProps } from "react";
 
 export interface IconProps extends SVGProps<SVGSVGElement> {
   size: number;
+  className?: string | undefined;
 }
 
 export const ArrowIcon: FC<IconProps> = (props) => {
-  const { size = 12, viewBox, ...rest } = props;
+  const { size = 12, className, viewBox, ...rest } = props;
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={`${size}px`}
       height={`${size}px`}
-      fill="none"
+      fill="currentColor"
       viewBox={`0 0 12 12`}
-      {...rest}
+      {...rest} 
+      className={`${className}`}
     >
       <path
-        fill="base-contrast-54"
+        
         d="M1.739 3.75a.24.24 0 0 0-.183.394l4.26 5.145a.238.238 0 0 0 .367 0l4.261-5.145a.24.24 0 0 0-.183-.394H1.739Z"
       />
     </svg>
