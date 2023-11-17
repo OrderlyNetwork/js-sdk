bc5a138c74fc97faa93fa8fc179ef07e4ef59791
Jame Zhan <jame@orderly.network>
2023-11-17 15:00:42 +0800

    update nav style

--- apps/docs/components/layout/navbar.tsx
+++ apps/docs/components/layout/navbar.tsx
@@ -1,14 +1,16 @@
 import { ConnectButton } from "../connectButton";
+import Link from "next/link";
 
 export const NavBar = () => {
   return (
-    <div className="h-[64px] shadow sticky top-0 z-10 bg-white">
-      <div className="flex flex-row items-center justify-between h-full max-w-[90rem] m-auto">
+    <div className="h-[65px] border-b-[1px] border-[rgb(229, 231, 235)] border-solid	 sticky top-0 z-10 bg-white">
+      <div className="flex flex-row items-center justify-between h-full max-w-[90rem] m-auto px-[24px]">
         <img src="/images/orderly.log.png" width={40} />
         <div className="flex gap-3 items-center">
           <ul className="flex gap-3">
-            <li>Document</li>
-            <li>APIs</li>
+            <li className="mr-[5px]"><Link  href="/docs/hooks/overview">Documentation
+            </Link></li>            
+            <li className="font-medium">API</li>
           </ul>
           {/* <ConnectButton /> */}
         </div>
