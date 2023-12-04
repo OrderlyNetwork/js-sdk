6540081cafeecf03f27844f7d24d90d4a045ed88
Leo <leo@orderly.network>
2023-12-04 17:54:13 +0800

    add data center

--- packages/hooks/src/services/dataCenter.ts
+++ packages/hooks/src/services/dataCenter.ts
@@ -1,15 +1,11 @@
 import { EventEmitter } from "@orderly.network/core";
 
 class DataCenter extends EventEmitter {
-    // private dataSource: any = new Map();
-    //
-
   constructor() {
     super();
   }
 
   observe = (key: string, callback: (value: any) => void) => {
-    // check if data exists, 
     // merge data
     this.on(key, callback);
   };
--- packages/hooks/src/useObserve.ts
+++ packages/hooks/src/useObserve.ts
@@ -5,19 +5,7 @@ export enum Subjects {
   Positions = "positions",
 }
 
-/**
- * observe subject
- * @param key subjectKey
- * @param initialValue 
- * 
- * @example
- * ```typescript
- * const [orders] = useObserve(Subjects.Orders, []);
- * ```
- */
 export const useObserve = <T>(
   key: string,
   initialValue: T
-): [T, (value: T) => void] => {
-    const [data, setData] = useState<T>(initialValue);
-};
+): [T, (value: T) => void] => {};
