197596f7546ce064f3f19c99daef9d6c40db9a02
tony <tony@woo.network>
2023-12-12 11:39:04 +0800

    add trading view

--- packages/component/src/page/trading/fill/myTradingview.tsx
+++ packages/component/src/page/trading/fill/myTradingview.tsx
@@ -1,7 +1,9 @@
+import { tradingView } from "@orderly.network/tradingView";
+
 export const MyTradingView = () => {
   return (
     <div className="orderly-flex orderly-items-center orderly-justify-center orderly-text-[150px] orderly-text-base-contrast/10 orderly-h-full">
-      Trading View
+      <TradingView />
     </div>
   );
 };
