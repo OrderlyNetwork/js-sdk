319c857926913f00d9fa1d3f42027919527d385d
Leo <leo@orderly.network>
2023-12-18 10:50:26 +0800

    update:  web-board add skip configuration parama

--- packages/onboard/src/provider.tsx
+++ packages/onboard/src/provider.tsx
@@ -7,18 +7,25 @@ import { Main } from "./main";
 export interface WalletConnectorProviderProps {
   apiKey?: string;
   options?: InitOptions;
+  // skip board configuration if already initialized
+  skipIfAlreadyInit?: boolean;
 }
 
 export const ConnectorProvider = (
   props: PropsWithChildren<WalletConnectorProviderProps>
 ) => {
-  const [initialized, setInitialized] = useState(false);
+  const [initialized, setInitialized] = useState(
+    !!props.skipIfAlreadyInit
+  );
 
   useEffect(() => {
     document.body.style.setProperty("--onboard-modal-z-index", "88");
   }, []);
 
   useEffect(() => {
+    if (props.skipIfAlreadyInit) {
+      return;
+    }
     initConfig(props.apiKey, props.options).then(() => {
       setInitialized(true);
     });
