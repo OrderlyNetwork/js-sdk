4257feb7ac77b6e5f9321124490e62ad97763be9
Mark <mark@orderly.network>
2024-01-04 14:52:59 +0800

    update order book content height calc

--- packages/component/src/page/trading/fill/sections/orderbook_trade/orderbook.tsx
+++ packages/component/src/page/trading/fill/sections/orderbook_trade/orderbook.tsx
@@ -18,7 +18,7 @@ interface MyOrderBookProps {
 const CELL_MAX = 30;
 const DEFAULT_CELL_HEIGHT = 20;
 
-const SPACE = 135;
+const SPACE = 122;
 
 export const MyOrderBook: FC<MyOrderBookProps> = (props) => {
   const { symbol } = props;
