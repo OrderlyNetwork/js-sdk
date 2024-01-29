dee474bec47e899c741244a2b02159754d4105de
Jame Zhan <jame@orderly.network>
2024-01-29 16:58:45 +0800

    fix onChainChange value is undefined

--- packages/component/src/block/deposit/depositForm.tsx
+++ packages/component/src/block/deposit/depositForm.tsx
@@ -309,6 +309,7 @@ export const DepositForm: FC<DepositFormProps> = (props) => {
 
   const onChainChange = useCallback(
     (value: API.Chain) => {
+      if (!value) return;
       if (value.network_infos?.chain_id === chain?.id) return Promise.resolve();
       return props
         .switchChain?.({
