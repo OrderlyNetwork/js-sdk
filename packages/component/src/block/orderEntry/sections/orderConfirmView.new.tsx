a93efda2e70aaf653a506dd0fd5fdd509fad4c23
Mark <mark@orderly.network>
2024-01-30 17:49:56 +0800

    modify order confirm view

--- packages/component/src/block/orderEntry/sections/orderConfirmView.new.tsx
+++ packages/component/src/block/orderEntry/sections/orderConfirmView.new.tsx
@@ -70,13 +70,13 @@ export const OrderConfirmView: FC<OrderConfirmViewProps> = (props) => {
             <span className="orderly-text-base-contrast-54">Qty.</span>
             <span>{order.order_quantity}</span>
           </div>
-          <div className="orderly-flex orderly-justify-between">
+          {order.isStopOrder === true && (<div className="orderly-flex orderly-justify-between">
             <span className="orderly-text-base-contrast-54">Trigger price</span>
             <div className="orderly-inline-block">
               <span>{order.trigger_price}</span>
               <span className="orderly-text-base-contrast-36 orderly-ml-1">{quote}</span>
             </div>
-          </div>
+          </div>)}
           <div className="orderly-flex orderly-justify-between">
             <span className="orderly-text-base-contrast-54">Price</span>
             {priceNode}
