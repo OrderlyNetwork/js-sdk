2e41dd3dcea324e6ec96888e6912e9c0cc0bfcc2
Jame Zhan <jame@orderly.network>
2024-02-01 11:08:22 +0800

    fix WsStatus z-index

--- packages/component/src/block/accountStatus/sections/WsStatus.tsx
+++ packages/component/src/block/accountStatus/sections/WsStatus.tsx
@@ -6,7 +6,7 @@ export const WsStatus: React.FC = () => {
     <div
       className={cn(
         "orderly-bg-warning-darken orderly-text-warning orderly-text-3xs orderly-font-semibold orderly-leading-[18px]",
-        "orderly-fixed orderly-left-0 orderly-right-0 orderly-z-[99]",
+        "orderly-fixed orderly-left-0 orderly-right-0 orderly-z-[30]",
         "orderly-flex orderly-items-center orderly-bottom-[64px] orderly-p-[10px]",
         "desktop:orderly-flex desktop:orderly-items-center desktop:orderly-justify-center desktop:orderly-static desktop:orderly-h-[40px] desktop:orderly-text-sm desktop:orderly-bottom-0"
       )}
@@ -25,4 +25,3 @@ export const WsStatus: React.FC = () => {
     </div>
   );
 };
-
