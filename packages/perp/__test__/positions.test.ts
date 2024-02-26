9933c139e2cc70305b860503b0f341a9888c7545
Leo <leo@orderly.network>
2024-02-26 15:57:06 +0800

    fix formula error

--- packages/perp/__test__/positions.test.ts
+++ packages/perp/__test__/positions.test.ts
@@ -11,7 +11,7 @@ describe("positions formula", () => {
         positionQty: 0,
         MMR: 0.5,
       };
-      expect(positions.liqPrice(inputs)).toBe(0);
+      expect(positions.liqPrice(inputs)).toBeNull();
     });
 
     it("should calculate the liqPrice correctly: BTC", () => {
@@ -60,6 +60,42 @@ describe("positions formula", () => {
       };
       expect(positions.liqPrice(inputs)).toBe(2106.993015873016);
     });
+
+    it("should calculate the liqPrice correctly: larger position BTC", () => {
+      const inputs: LiqPriceInputs = {
+        markPrice: 51000,
+        totalCollateral: 27000,
+        positions: [
+          // BTC
+          {
+            position_qty: 5,
+            mark_price: 51000,
+            mmr: 0.025,
+          },
+        ],
+        positionQty: 5,
+        MMR: 0.025,
+      };
+      expect(positions.liqPrice(inputs)).toBe(46769.230769230766);
+    });
+
+    it("should calculate the liqPrice correctly: larger position BTC -- case 2", () => {
+      const inputs: LiqPriceInputs = {
+        markPrice: 51000,
+        totalCollateral: 270000,
+        positions: [
+          // BTC
+          {
+            position_qty: 100,
+            mark_price: 51000,
+            mmr: 0.0408506069,
+          },
+        ],
+        positionQty: 100,
+        MMR: 0.0408506069,
+      };
+      expect(positions.liqPrice(inputs)).toBe(50357.11886747166);
+    });
   });
 
   describe("MMR", () => {
@@ -85,6 +121,19 @@ describe("positions formula", () => {
         IMR_factor_power: 4 / 5,
       };
       expect(positions.MMR(inputs)).toBe(0.05);
+    });
+
+    it("should calculate the MMR correctly: case 1", () => {
+      //ETH
+      const inputs: MMRInputs = {
+        baseMMR: 0.025,
+        baseIMR: 0.05,
+        IMRFactor: 0.0000003517,
+        positionNotional: 255000,
+        IMR_factor_power: 4 / 5,
+
+      };
+      expect(positions.MMR(inputs)).toBe(0.025);
     });
   });
 });
