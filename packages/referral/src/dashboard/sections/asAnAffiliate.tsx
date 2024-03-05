0e7b62bdb8ed25c34ab7206fc884bb7657395e09
Jame Zhan <jame@orderly.network>
2024-03-05 14:23:05 +0800

    remove @/ import

--- packages/referral/src/dashboard/sections/asAnAffiliate.tsx
+++ packages/referral/src/dashboard/sections/asAnAffiliate.tsx
@@ -1,28 +1,27 @@
-import { ReferralIcon } from "@/icons/referral";
-import { Button } from "@orderly.network/react"
+import { ReferralIcon } from "../../icons/referral";
+import { Button } from "@orderly.network/react";
 
 export const AsAnAffiliate = () => {
-
-    return (
-        <div className="orderly-rounded-lg orderly-p-6 orderly-bg-primary">
-            <div className="orderly-flex">
-                <div className="orderly-justify-between">
-                <div>As an affiliate</div>
-                <div className="orderly-mt-6">Onboard traders to earn passive income</div>
-                </div>
-                <ReferralIcon />
-            </div>
-            
-            <div className="orderly-flex orderly-justify-between orderly-mt-2">
-                <Button className="orderly-h-[44px]">
-                    Become an affiliate
-                </Button>
-
-                <div>
-                    <div>40%~80%</div>
-                    <div>Commission</div>
-                </div>
-            </div>
+  return (
+    <div className="orderly-rounded-lg orderly-p-6 orderly-bg-primary">
+      <div className="orderly-flex">
+        <div className="orderly-justify-between">
+          <div>As an affiliate</div>
+          <div className="orderly-mt-6">
+            Onboard traders to earn passive income
+          </div>
         </div>
-    );
-}
+        <ReferralIcon />
+      </div>
+
+      <div className="orderly-flex orderly-justify-between orderly-mt-2">
+        <Button className="orderly-h-[44px]">Become an affiliate</Button>
+
+        <div>
+          <div>40%~80%</div>
+          <div>Commission</div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- packages/referral/src/dashboard/sections/earn.tsx
+++ packages/referral/src/dashboard/sections/earn.tsx
@@ -1,7 +1,12 @@
-
-import { ShareIcon } from "@/icons/share";
+import { ShareIcon } from "../../icons/share";
 import { ListTile } from "./listTile";
 
 export const Earn = () => {
-    return <ListTile icon={(<ShareIcon />)} title="Earn" subtitle="Get paid and receive special treatment 24/7, 365"/>;
-}
+  return (
+    <ListTile
+      icon={<ShareIcon />}
+      title="Earn"
+      subtitle="Get paid and receive special treatment 24/7, 365"
+    />
+  );
+};
