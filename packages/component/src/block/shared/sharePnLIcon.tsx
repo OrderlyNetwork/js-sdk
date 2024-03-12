b4e54481c90dec9d37c31d2f7261628b71377c4c
Mark <mark@orderly.network>
2024-03-12 11:46:49 +0800

    open share pnl entry

--- packages/component/src/block/shared/sharePnLIcon.tsx
+++ packages/component/src/block/shared/sharePnLIcon.tsx
@@ -10,20 +10,19 @@ export const SharePnLIcon: FC<PropsWithChildren<{
     position: any,
 }>> = (props) => {
 
-    return (<></>);
-    
-    // return (<PositionShareIcon
-    //     size={12}
-    //     className={
-    //         cn("orderly-fill-white/20 hover:orderly-fill-white/80 hover:orderly-cursor-pointer orderly-inline-block", props.className)
-    //     }
-    //     fill="current"
-    //     fillOpacity={1}
-    //     onClick={() => {
-            
-    //         modal.show(SharePoisitionView, {
-    //             position: props.position,
-    //         });
-    //     }}
-    // />);
+
+    return (<PositionShareIcon
+        size={12}
+        className={
+            cn("orderly-fill-white/20 hover:orderly-fill-white/80 hover:orderly-cursor-pointer orderly-inline-block", props.className)
+        }
+        fill="current"
+        fillOpacity={1}
+        onClick={() => {
+
+            modal.show(SharePoisitionView, {
+                position: props.position,
+            });
+        }}
+    />);
 }
