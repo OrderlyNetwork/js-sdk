9360ef1f37d6c7e40426d0bcf171fcc94a44a748
Mark <mark@orderly.network>
2024-03-29 17:17:45 +0800

    issue-935

--- packages/component/src/block/walletConnect/sections/referralCode.tsx
+++ packages/component/src/block/walletConnect/sections/referralCode.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/input";
 import { OrderlyAppContext } from "@/provider";
 import { cn } from "@/utils";
 import { OrderlyContext, useLocalStorage } from "@orderly.network/hooks";
-import { FC, useContext, useState } from "react";
+import { FC, useContext, useEffect, useRef, useState } from "react";
 
 export const ReferralCode: FC<{
     className?: string,
@@ -11,24 +11,37 @@ export const ReferralCode: FC<{
     setRefCode: any,
 }> = (props) => {
 
-    const { className, refCode ,setRefCode } = props;
+    const { className, refCode, setRefCode } = props;
 
     const { saveRefCode } = useContext(OrderlyContext);
 
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+        if (saveRefCode && inputRef) {
+
+            inputRef?.current?.focus();
+            inputRef?.current?.setSelectionRange(inputRef?.current.value.length, inputRef.current.value.length);
+
+        }
+    }, [saveRefCode, refCode, inputRef])
+
     console.log("xxxxxxxxxxxx saveRefCode", saveRefCode);
-    
 
 
-    if (!refCode || !saveRefCode) return <></>;
+
+    if (!saveRefCode) return <></>;
 
     return (
         <div className={cn("orderly-text-2xs orderly-text-base-contrast-80", props.className)}>
             <div>Referral code (optional)</div>
             <div className="orderly-pt-3">
                 <Input
+                    ref={inputRef}
                     containerClassName="orderly-h-[40px] orderly-bg-base-900"
                     value={refCode}
                     onChange={(e) => setRefCode(e.target.value)}
+                    autoFocus
                     suffix={<button className="orderly-mr-2" onClick={(e) => {
                         e.stopPropagation();
                         setRefCode("");
