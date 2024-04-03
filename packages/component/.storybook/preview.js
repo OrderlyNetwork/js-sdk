40d580617b4ea49676db23d6acbc2a916ae6ac04
Leo <leo@orderly.network>
2024-04-03 16:56:28 +0800

    remove some API keys

--- packages/component/.storybook/preview.js
+++ packages/component/.storybook/preview.js
@@ -10,10 +10,6 @@ import { CustomContractManager } from "./CustomContract";
 import { CustomConfigStore } from "./CustomConfigStore";
 import "../src/tailwind.css"; // tailwind css
 
-const apiKey = "a2c206fa-686c-466c-9046-433ea1bf5fa6";
-const FujiRpcUrl = "https://api.avax-test.network/ext/bc/C/rpc";
-const INFURA_KEY = "3039f275d050427d8859a728ccd45e0c";
-
 const wcV2InitOptions = {
   version: 2,
   projectId: "93dba83e8d9915dc6a65ffd3ecfd19fd",
