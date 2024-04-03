86d351e2857c18856e117436693242fd1ee9dfa9
Mario Reder <mario.reder@pm.me>
2024-04-03 14:57:58 +0200

    fix: useConfig return type

--- packages/hooks/src/useConfig.ts
+++ packages/hooks/src/useConfig.ts
@@ -2,18 +2,26 @@ import { useContext } from "react";
 import { OrderlyContext } from "./orderlyContext";
 import { ConfigStore, type ConfigKey } from "@orderly.network/core";
 
-export const useConfig = <T>(
-  key?: ConfigKey,
+export function useConfig<K extends ConfigKey | undefined, T>(
+  key?: K,
   defaultValue?: T
-): T | ConfigStore => {
+): undefined extends K
+  ? ConfigStore
+  : undefined extends T
+  ? any
+  : T extends infer R
+  ? R
+  : never {
   const { configStore } = useContext(OrderlyContext);
 
   if (typeof key !== "undefined") {
     if (typeof defaultValue !== "undefined") {
-      return configStore.getOr(key, defaultValue);
+      // TODO fix type guard
+      return configStore.getOr(key, defaultValue) as any;
     }
     return configStore.get(key);
   }
 
-  return configStore;
-};
+  // TODO fix type guard
+  return configStore as any;
+}
