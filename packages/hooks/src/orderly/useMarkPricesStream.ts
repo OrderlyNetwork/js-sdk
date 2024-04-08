0741c6b410cc881621cb4c911bc6d327116670b3
Mario Reder <mario.reder@pm.me>
2024-04-08 15:35:56 +0200

    doc: export types for typedoc

--- packages/hooks/src/orderly/useMarkPricesStream.ts
+++ packages/hooks/src/orderly/useMarkPricesStream.ts
@@ -1,32 +1,38 @@
-import useSWRSubscription from "swr/subscription";
+import useSWRSubscription, { SWRSubscriptionOptions } from "swr/subscription";
 import { useWS } from "../useWS";
 
 export const useMarkPricesStream = () => {
   const ws = useWS();
-  return useSWRSubscription("markPrices", (key, { next }) => {
-    const unsubscribe = ws.subscribe(
-      // { event: "subscribe", topic: "markprices" },
-      "markprices",
-      {
-        onMessage: (message: any) => {
-          const data: Record<string, number> = Object.create(null);
+  return useSWRSubscription<Record<string, number>>(
+    "markPrices",
+    (
+      key: string,
+      { next }: SWRSubscriptionOptions<Record<string, number>, any>
+    ) => {
+      const unsubscribe = ws.subscribe(
+        // { event: "subscribe", topic: "markprices" },
+        "markprices",
+        {
+          onMessage: (message: any) => {
+            const data: Record<string, number> = Object.create(null);
 
-          for (let index = 0; index < message.length; index++) {
-            const element = message[index];
-            data[element.symbol] = element.price;
-          }
+            for (let index = 0; index < message.length; index++) {
+              const element = message[index];
+              data[element.symbol] = element.price;
+            }
 
-          next(null, data);
-        },
-        // onUnsubscribe: () => {
-        //   return "markprices";
-        // },
-        onError: (error: any) => {},
-      }
-    );
+            next(null, data);
+          },
+          // onUnsubscribe: () => {
+          //   return "markprices";
+          // },
+          onError: (error: any) => {},
+        }
+      );
 
-    return () => {
-      unsubscribe?.();
-    };
-  });
+      return () => {
+        unsubscribe?.();
+      };
+    }
+  );
 };
