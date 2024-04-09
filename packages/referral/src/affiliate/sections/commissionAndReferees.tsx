317ab967a4e8aacf61099460820829b05670b7b8
Mark <mark@orderly.network>
2024-04-09 23:17:11 +0800

    update

--- packages/referral/src/affiliate/sections/commissionAndReferees.tsx
+++ packages/referral/src/affiliate/sections/commissionAndReferees.tsx
@@ -25,7 +25,7 @@ export const CommissionAndReferees: FC<{ className?: string }> = (props) => {
                 onTabChange={onTabChange}
                 tabBarClassName="orderly-referral-tab-bar orderly-h-[61px] orderly-text-xs md:orderly-text-base 2xl:orderly-text-lg orderly-px-2 lg:orderly-px-4"
                 tabBarExtra={
-                    <div className="orderly-mt-1 orderly-px-4 orderly-py-2 sm:orderly-flex orderly-items-center orderly-invisible orderly-w-0 md:orderly-visible md:orderly-w-auto orderly-text-3xs orderly-text-base-contrast-36">{dateText}</div>
+                    <div className="orderly-mt-1 orderly-px-4 lg:orderly-mr-[2px] orderly-py-2 sm:orderly-flex orderly-items-center orderly-invisible orderly-w-0 md:orderly-visible md:orderly-w-auto orderly-text-3xs orderly-text-base-contrast-36">{dateText}</div>
                 }
                 identifierClassName={"after:orderly-bg-gradient-to-l after:orderly-from-referral-text-from after:orderly-to-referral-text-to"}
             >
