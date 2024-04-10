f7485fc4f17fbcfca21cc9b0ea9dd0763b41415c
Mark <mark@orderly.network>
2024-04-10 16:50:26 +0800

    Update referralLink.tsx

--- packages/referral/src/affiliate/sections/referralLink.tsx
+++ packages/referral/src/affiliate/sections/referralLink.tsx
@@ -164,7 +164,7 @@ const CopyInfo: FC<{ title: string, value: string | React.ReactNode, copyText: s
     return (
         <div className="orderly-flex orderly-p-3 orderly-items-center orderly-bg-base-500 orderly-rounded-md orderly-justify-between">
             <div className="orderly-text-base-contrast-54 orderly-text-3xs lg:orderly-text-2xs 2xl:orderly-text-xs">{title}</div>
-            <div className="orderly-flex-1 orderly-flex">
+            <div className="orderly-flex-1 orderly-flex orderly-items-center">
                 <div className="orderly-flex-1 orderly-mx-3 orderly-text-right orderly-text-xs 2xl:orderly-text-base">{value}</div>
                 <CopyIcon
                     fillOpacity={1}
