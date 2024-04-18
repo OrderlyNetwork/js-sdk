ee727dfc15a8555ee215173f007593963ee78e2b
Leo <leo@orderly.network>
2024-04-18 10:36:18 +0800

    remove comment

--- packages/utils/src/decimal.ts
+++ packages/utils/src/decimal.ts
@@ -41,15 +41,6 @@ export function toNonExponential(num: number) {
   return num.toFixed(
     Math.max(0, (m[1] || "").length - (m[2] as unknown as number))
   );
-
-  //  //处理非数字
-  //  if(isNaN(num)){return num};
-
-  //  //处理不需要转换的数字
-  //  var str = ''+num;
-  //  if(!/e/i.test(str)){return num;};
-
-  //  return (num).toFixed(18).replace(/\.?0+$/, "");
 }
 
 /**
