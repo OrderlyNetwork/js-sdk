a45cf3b2d5a5be40197e14bcada0cc5ed270bb74
Jame Zhan <jame@orderly.network>
2024-04-26 18:48:00 +0800

    fix addEventListener is undefined

--- packages/hooks/src/useMediaQuery.ts
+++ packages/hooks/src/useMediaQuery.ts
@@ -25,14 +25,14 @@ export function useMediaQuery(query: string): boolean {
     if (matchMedia?.addListener) {
       matchMedia?.addListener(handleChange);
     } else {
-      matchMedia?.addEventListener("change", handleChange);
+      matchMedia?.addEventListener?.("change", handleChange);
     }
 
     return () => {
       if (matchMedia?.removeListener) {
         matchMedia?.removeListener(handleChange);
       } else {
-        matchMedia?.removeEventListener("change", handleChange);
+        matchMedia?.removeEventListener?.("change", handleChange);
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
