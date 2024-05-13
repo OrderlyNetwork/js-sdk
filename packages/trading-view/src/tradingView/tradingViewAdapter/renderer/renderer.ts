78ac73b7601863b16e2767ae488cbc96927d52f5
tony <tony@woo.network>
2024-05-13 15:08:15 +0800

    fix render

--- packages/trading-view/src/tradingView/tradingViewAdapter/renderer/renderer.ts
+++ packages/trading-view/src/tradingView/tradingViewAdapter/renderer/renderer.ts
@@ -12,8 +12,7 @@ export class Renderer{
         this.instance = instance;
         this.positionLineService = new PositionLineService(instance, broker);
         this.orderLineService = new OrderLineService(instance, broker);
-        this.preventDefaultRenderHack(host);
-        this.forceSilentOrdersPlacement(host);
+
     }
 
     async renderPositions(positions: ChartPosition[] | null) {
@@ -71,52 +70,4 @@ export class Renderer{
             }),
         );
     }
-
-    // We implement our own render logic and skip TradingView's default render
-    preventDefaultRenderHack(host: IBrokerConnectionAdapterHost) {
-        (host as any).setBrokerConnectionAdapter = function (adapter: any) {
-            const delegate = {
-                subscribe: () => {},
-                unsubscribe: () => {},
-                unsubscribeAll: () => {},
-            };
-
-            Object.defineProperty(adapter, '_ordersCache', {
-                get: function () {
-                    return {
-                        start: () => {},
-                        stop: () => {},
-                        update: () => {},
-                        partialUpdate: () => {},
-                        fullUpdate: () => {},
-                        getObjects: async () => [],
-                        updateDelegate: delegate,
-                        partialUpdateDelegate: delegate,
-                    };
-                },
-                set: () => {},
-            });
-            adapter._waitForOrderModification = async () => true;
-            this._adapter = adapter;
-        };
-    }
-
-    forceSilentOrdersPlacement = (host: any) => {
-        this.instance.onChartReady(() => {
-            // Apply default teamplate makes the silentOrdersPlacement disabled, need to set it back
-            host.silentOrdersPlacement().subscribe((val: any) => {
-                if (!val) {
-                    host.silentOrdersPlacement().setValue(true);
-                    if (this.instance) {
-                        (this.instance as any)._iFrame.contentDocument.querySelector('.wrapper-3X2QgaDd').className =
-                            'wrapper-3X2QgaDd highButtons-3X2QgaDd';
-                    }
-
-                    // The block can only be executed while "Apply default teamplate".
-                    // We don't wnat to show btn buy defualt, so close it.
-                    host.sellBuyButtonsVisibility()?.setValue(false);
-                }
-            });
-        });
-    };
 }
