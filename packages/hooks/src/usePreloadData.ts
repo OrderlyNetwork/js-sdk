f61f7414b4ba3abd83b559aae45000e981424994
Mark <mark@orderly.network>
2024-05-16 17:03:53 +0800

    fix request api error when device timestamp wrong

--- packages/hooks/src/usePreloadData.ts
+++ packages/hooks/src/usePreloadData.ts
@@ -1,5 +1,7 @@
 import { useMemo } from "react";
-import { useQuery } from ".";
+import { useConfig, useQuery, useSWR } from ".";
+import { request } from "@orderly.network/net/src/fetch";
+import { getGlobalObject } from "@orderly.network/utils";
 
 export const usePreLoadData = () => {
   const { error: tokenError, data: tokenData } = useQuery(
@@ -7,6 +9,42 @@ export const usePreLoadData = () => {
     {
       revalidateOnFocus: false,
     }
+  );
+
+  /// get service timestamp
+  /// get local timestamp
+  /// calculate delta offset = SD - LD
+  /// save to getGlobalObject.__ORDERLY_timestamp_offset
+
+  const apiBaseUrl = useConfig("apiBaseUrl");
+
+  const { data: systemInfo } = useSWR(
+    "/v1/public/system_info",
+    async (url: any, init: any) => {
+      const data = await fetch(
+        url.startsWith("http") ? url : `${apiBaseUrl}${url}`,
+        init
+      );
+      return await data.json();
+    },
+    {
+      errorRetryCount: 3,
+      errorRetryInterval: 500,
+    }
+  );
+
+  if (typeof systemInfo !== "undefined") {
+    const sd = systemInfo.timestamp;
+    const ld = Date.now();
+    // @ts-ignore
+    getGlobalObject().__ORDERLY_timestamp_offset = sd - ld;
+  }
+
+  // @ts-ignore
+  console.log(
+    "systemInfo",
+    systemInfo,
+    getGlobalObject().__ORDERLY_timestamp_offset
   );
 
   const isDone = useMemo(() => {
