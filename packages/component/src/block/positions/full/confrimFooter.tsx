e352b4d6b82ed19f410670a3e465001ce1f06b74
Mark <mark@orderly.network>
2024-05-20 10:47:16 +0800

    modify id

--- packages/component/src/block/positions/full/confrimFooter.tsx
+++ packages/component/src/block/positions/full/confrimFooter.tsx
@@ -10,10 +10,10 @@ export const ConfirmFooter: FC<{
     <div id="orderly-positions-confrim-fotter" className="orderly-grid orderly-grid-cols-[1fr_2fr] orderly-mt-5">
       <div />
       <div className="orderly-flex orderly-gap-2">
-        <Button id="orderly-cancel-button" color={"tertiary"} fullWidth onClick={onCancel}>
+        <Button id="orderly-positions-cancel-button" color={"tertiary"} fullWidth onClick={onCancel}>
           Cancel
         </Button>
-        <Button id="orderly-confirm-button" onClick={onConfirm} fullWidth loading={submitting}>
+        <Button id="orderly-positions-confirm-button" onClick={onConfirm} fullWidth loading={submitting}>
           Confirm
         </Button>
       </div>
