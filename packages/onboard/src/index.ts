5a22840939ca1585538008a6497fefe039d67440
Leo <leo@orderly.network>
2024-06-21 10:38:23 +0800

    fix missing export

--- packages/onboard/src/index.ts
+++ packages/onboard/src/index.ts
@@ -2,3 +2,4 @@ export { default as version } from "./version";
 export { ConnectorProvider, type ConnectorInitOptions } from "./provider";
 export type { InitOptions, OnboardAPI } from "@web3-onboard/core";
 export { default as Onboard } from "@web3-onboard/core";
+export { initConfig } from "./config";
