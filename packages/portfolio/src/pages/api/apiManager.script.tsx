82754fe575e2795994a8888d1a90c558f0dc8f06
Mark <mark@orderly.network>
2024-07-16 15:01:16 +0800

    api key top ui

--- packages/portfolio/src/pages/api/apiManager.script.tsx
+++ packages/portfolio/src/pages/api/apiManager.script.tsx
@@ -1,6 +1,18 @@
-export type ApiManagerScriptReturns = {};
+export type ApiManagerScriptReturns = {
+    address?: string;
+    uid?: number;
+    onCreateApiKey?: () => void;
+    onReadApiGuide?: () => void;
+};
 
 export const useApiManagerScript = (): ApiManagerScriptReturns => {
 
-    return {};
+    const onCreateApiKey = () => {};
+    const onReadApiGuide = () => {};
+    return {
+        address: "0xe8f299b9555c6de49fd9b06637cd89781901111f",
+        uid: 106732112,
+        onCreateApiKey,
+        onReadApiGuide
+    };
 };
--- packages/portfolio/src/pages/api/apiManager.ui.tsx
+++ packages/portfolio/src/pages/api/apiManager.ui.tsx
@@ -1,22 +1,124 @@
-import { Card, Divider, Flex,Text } from "@orderly.network/ui";
+import {
+  ArrowLeftRightIcon,
+  Button,
+  Card,
+  DataTable,
+  Divider,
+  Flex,
+  PlusIcon,
+  Text,
+} from "@orderly.network/ui";
+import { ApiManagerScriptReturns } from "./apiManager.script";
+import { FC } from "react";
 
-export const APIManager = () => {
+export const APIManager: FC<ApiManagerScriptReturns> = (props) => {
   return (
-    <Card title={"API keys"} id="portfolio-apikey-manager" className="oui-bg-base-9">
-      
-      <Flex>
-        <Divider/>
-        <Card title={"Account ID"} className="oui-bg-base-7">
-          <Text.formatted rule={"address"} copyable>
-            {"0xasfsfsdfsdfsdfdfs"}
-          </Text.formatted>
-        </Card>
-        <Card title={"Account ID"}>
-          <Text.formatted rule={"address"} copyable>
-            {"0xasfsfsdfsdfsdfdfs"}
-          </Text.formatted>
-        </Card>
+    <Card
+      title={"API keys"}
+      id="portfolio-apikey-manager"
+      className="oui-bg-base-9"
+    >
+      <Flex direction={"column"} gap={4} width={"100%"}>
+        <AccountInfo {...props} />
+        <Subtitle {...props} />
+        <KeyList {...props} />
       </Flex>
     </Card>
+  );
+};
+
+const AccountInfo: FC<ApiManagerScriptReturns> = (props) => {
+  return (
+    <Flex
+      width={"100%"}
+      gap={4}
+      className="oui-border-t-2 oui-border-line-6 oui-pt-4"
+    >
+      <Flex
+        py={2}
+        px={4}
+        direction={"column"}
+        itemAlign={"start"}
+        r="xl"
+        gradient="neutral"
+        angle={27}
+        className="oui-w-1/2"
+      >
+        <Text size="xs" intensity={36}>
+          Account ID
+        </Text>
+        <Text.formatted size="base" inlist={80} rule={"address"} copyable>
+          {props.address}
+        </Text.formatted>
+      </Flex>
+      <Flex
+        py={2}
+        px={4}
+        direction={"column"}
+        itemAlign={"start"}
+        r="xl"
+        gradient="neutral"
+        angle={27}
+        className="oui-w-1/2"
+      >
+        <Text size="xs" intensity={36}>
+          UID
+        </Text>
+        <Text size="base" inlist={80}>
+          {props.uid}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+const Subtitle: FC<ApiManagerScriptReturns> = (props) => {
+  return (
+    <Flex
+      width={"100%"}
+      direction={"row"}
+      className="oui-text-sm oui-border-b-2 oui-border-line-6 oui-pb-4"
+    >
+      <Flex
+        direction={"column"}
+        itemAlign={"start"}
+        width={"100%"}
+        gap={1}
+        onClick={props.onReadApiGuide}
+      >
+        <Text intensity={54}>
+          Create API keys to suit your trading needs. For your security, don't
+          share your API keys with anyone.{" "}
+        </Text>
+        <Flex className="oui-text-primary oui-fill-white/[.54] hover:oui-text-primary-light hover:oui-fill-white/[.8] oui-cursor-pointer">
+          <Text>Read API guide</Text>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            fillOpacity="1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M6.777 3.348a.65.65 0 0 0-.498.083.685.685 0 0 0-.187.938L8.5 7.993 6.092 11.62a.685.685 0 0 0 .187.937.68.68 0 0 0 .934-.187l2.657-4a.69.69 0 0 0 0-.75l-2.657-4a.67.67 0 0 0-.436-.271" />
+          </svg>
+        </Flex>
+      </Flex>
+      <Button
+        size="md"
+        icon={<PlusIcon />}
+        variant="contained"
+        color="primary"
+        onClick={props.onCreateApiKey}
+      >
+        Create API key
+      </Button>
+    </Flex>
+  );
+};
+
+const KeyList: FC<ApiManagerScriptReturns> = (props) => {
+  return (
+      <DataTable />
   );
 };
