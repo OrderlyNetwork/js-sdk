70a1a037157acf285c69961c94e7f5915f0b9fea
tony <tony@woo.network>
2024-07-17 10:28:19 +0800

    dex-1229

---
+++ packages/component/src/page/trading/desktop/elements/switchMarginModulePlace.tsx
@@ -0,0 +1,10 @@
+import { SwitchOperateIcon } from "@/icon/icons/switchOperateIcon";
+
+export default function SwitchMarginModulePlace() {
+  return (
+    <div className="orderly-absolute orderly-top-4 orderly-right-0">
+      <SwitchOperateIcon />
+      <div className="orderly-w-6 orderly-h-8 orderly-border orderly-border-solid orderly-border-primary-12 orderly-rounded-sm"></div>
+    </div>
+  );
+}
