91caa0d8e94f9b9926a27e449559ac9df0efc87b
Leo <leo@orderly.network>
2024-07-29 14:34:14 +0800

    ui-chain-selector add network config, support only mainnet or testnet

--- packages/ui-chain-selector/src/chainSelector.script.ts
+++ packages/ui-chain-selector/src/chainSelector.script.ts
@@ -1,6 +1,11 @@
 import { useChains, useWalletConnector } from "@orderly.network/hooks";
+import { NetworkId } from "@orderly.network/types";
+import { useMemo } from "react";
 
-export const useChainSelectorBuilder = () => {
+export const useChainSelectorBuilder = (options?: {
+  networkId?: NetworkId;
+}) => {
+  const { networkId } = options || {};
   const [chains] = useChains();
   const { setChain, connectedChain } = useWalletConnector();
 
@@ -13,8 +18,8 @@ export const useChainSelectorBuilder = () => {
     });
   };
 
-  return {
-    chains: {
+  const filteredChains = useMemo(() => {
+    const _chains = {
       mainnet: chains.mainnet.map((chain) => ({
         name: chain.network_infos.name,
         id: chain.network_infos.chain_id,
@@ -25,7 +30,25 @@ export const useChainSelectorBuilder = () => {
         id: chain.network_infos.chain_id,
         lowestFee: chain.network_infos.bridgeless,
       })),
-    },
+    };
+
+    if (typeof networkId === "undefined") {
+      return _chains;
+    }
+
+    if (networkId === "testnet") {
+      return {
+        testnet: _chains.testnet,
+      };
+    }
+
+    return {
+      mainnet: _chains.mainnet,
+    };
+  }, [chains, networkId]);
+
+  return {
+    chains: filteredChains,
     onChainChange,
     currentChainId: connectedChain?.id as number | undefined,
   };
--- packages/ui-chain-selector/src/chainSelector.widget.tsx
+++ packages/ui-chain-selector/src/chainSelector.widget.tsx
@@ -1,9 +1,13 @@
 import { registerSimpleDialog } from "@orderly.network/ui";
 import { useChainSelectorBuilder } from "./chainSelector.script";
 import { ChainSelector } from "./chainSelector.ui";
+import { NetworkId } from "@orderly.network/types";
 
-export const ChainSelectorWidget = (props: { close?: () => void }) => {
-  const state = useChainSelectorBuilder();
+export const ChainSelectorWidget = (props: {
+  networkId?: NetworkId;
+  close?: () => void;
+}) => {
+  const state = useChainSelectorBuilder(props);
   return <ChainSelector {...state} close={props.close} />;
 };
 
