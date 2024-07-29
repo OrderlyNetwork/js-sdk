c495b692e0869d395ce77bbd09714690bcba3a40
Jame Zhan <jame@orderly.network>
2024-07-29 21:02:33 +0800

    update ui-transfer export

--- packages/ui-transfer/src/index.ts
+++ packages/ui-transfer/src/index.ts
@@ -1,3 +1,13 @@
 export * from "./components/depositForm";
 export * from "./components/depositAndWithdraw";
-export * from './components/withdrawForm';
+export * from "./components/withdrawForm";
+
+export { ChainSelect } from "./components/chainSelect";
+export { QuantityInput } from "./components/quantityInput";
+export { AvailableQuantity } from "./components/availableQuantity";
+export { Web3Wallet } from "./components/web3Wallet";
+export { BrokerWallet } from "./components/brokerWallet";
+export { ExchangeDivider } from "./components/exchangeDivider";
+export { CoinExchange } from "./components/coinExchange";
+export { ActionButton } from "./components/actionButton";
+export { Fee } from "./components/fee";
