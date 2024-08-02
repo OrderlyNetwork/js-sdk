7a8ff4eda30b993fc8344b24d15eaf4050f4a861
Mark <mark@orderly.network>
2024-08-02 15:49:49 +0800

    remove purple gradient

--- packages/ui/src/tailwind/gradient.ts
+++ packages/ui/src/tailwind/gradient.ts
@@ -31,19 +31,5 @@ export const gradientPlugin = () =>
         backgroundImage:
           "linear-gradient(var(--oui-gradient-angle, 270deg), rgb(var(--oui-gradient-brand-start)) 0%, rgb(var(--oui-gradient-brand-end)) 100%)",
       },
-      ".gradient-purpe": {
-        backgroundImage:
-          "linear-gradient(var(--oui-gradient-angle, 28.29deg), rgb(var(--oui-gradient-purpe-start)) 0%, rgb(var(--oui-gradient-purpe-end)) 100%)",
-      },
     });
   });
-/*
-background: linear-gradient(28.29deg, #1828C3 21.6%, #608CFF 83.23%);
-background: linear-gradient(28.29deg, #005A4F 21.6%, #00B49E 83.23%);
-background: linear-gradient(28.29deg, #792E00 21.6%, #FFB65D 83.23%);
-background: linear-gradient(28.29deg, #791438 21.6%, #FF447C 83.23%);
-background: linear-gradient(28.29deg, #1B1D22 21.6%, #26292E 83.23%);
-background: linear-gradient(28.29deg, #2D0061 21.6%, #BD6BED 83.23%);
-background: linear-gradient(28.29deg, #2D0061 21.6%, #BD6BED 83.23%);
-
-*/
