eb76c30362ea2b07c2f9c9e0687148c42c5d36b5
Mark <mark@orderly.network>
2024-08-13 19:12:41 +0800

    bar chart and dataTable

--- packages/chart/src/orderly/volBar.tsx
+++ packages/chart/src/orderly/volBar.tsx
@@ -143,7 +143,7 @@ export const VolBarChart = (props: VolChartProps) => {
           {/* @ts-ignore */}
           <YAxis
             tick={{ fontSize: 10, fill: "rgba(255,255,255,0.54)" }}
-            tickLine={true}
+            tickLine={false}
             axisLine={false}
             tickCount={6}
             dataKey={"volume"}
