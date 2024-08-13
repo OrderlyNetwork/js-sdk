24dd353df989cfad3ee424aa56c0d3a9b95f340b
Mark <mark@orderly.network>
2024-08-13 15:21:44 +0800

    update reduce only click area

--- packages/component/src/block/orderEntry/sections/reduceOnly.tsx
+++ packages/component/src/block/orderEntry/sections/reduceOnly.tsx
@@ -1,7 +1,13 @@
 import { Label } from "@/label";
 import { modal } from "@/modal";
 import { FC, useCallback, useState } from "react";
-import { Tooltip, TooltipArrow, TooltipContent, TooltipTrigger } from "@radix-ui/react-tooltip";
+import {
+  Tooltip,
+  TooltipArrow,
+  TooltipContent,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import { OrderEntity } from "@orderly.network/types";
 
 export const MobileReduceOnlyLabel: FC = () => {
   const showReduceOnlyHint = useCallback(() => {
@@ -17,43 +23,55 @@ export const MobileReduceOnlyLabel: FC = () => {
     });
   }, []);
 
-  return (<>
-    <Label
-      className="orderly-text-base-contrast-54"
-      onClick={() => {
-        showReduceOnlyHint();
-      }}
-    >
-      Reduce only
-    </Label>
-  </>);
-}
+  return (
+    <>
+      <Label
+        className="orderly-text-base-contrast-54"
+        onClick={() => {
+          showReduceOnlyHint();
+        }}
+      >
+        Reduce only
+      </Label>
+    </>
+  );
+};
 
-export const DesktopReduceOnlyLabel: FC = () => {
+export const DesktopReduceOnlyLabel: FC<{
+  reduceOnly?: boolean;
+  onFieldChange: (name: keyof OrderEntity, value: any) => void;
+}> = (props) => {
   const [open, setOpen] = useState(false);
 
-  return (<>
-    <Tooltip>
-      <TooltipTrigger>
-        <Label
-          className="orderly-text-base-contrast-54"
+  return (
+    <>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <button
+            className="orderly-text-base-contrast-54"
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              props.onFieldChange("reduce_only", !props.reduceOnly);
+            }}
+          >
+            Reduce only
+          </button>
+        </TooltipTrigger>
+        <TooltipContent
+          align="center"
+          className="orderly-max-w-[300px] orderly-z-20 orderly-text-base-contrast orderly-select-none orderly-rounded orderly-bg-base-400 orderly-p-3 orderly-text-4xs"
         >
-          Reduce only
-        </Label>
-      </TooltipTrigger>
-      <TooltipContent
-        align="center"
-        className="orderly-max-w-[300px] orderly-z-20 orderly-text-base-contrast orderly-select-none orderly-rounded orderly-bg-base-400 orderly-p-3 orderly-text-4xs"
-      >
-        <div>
-          <span className="orderly-text-3xs">
-            Reduce only ensures that you can only reduce or close a current
-            position so that your position size will not be increased
-            unintentionally.
-          </span>
-        </div>
-        <TooltipArrow className="orderly-fill-base-400" />
-      </TooltipContent>
-    </Tooltip>
-  </>);
+          <div>
+            <span className="orderly-text-3xs">
+              Reduce only ensures that you can only reduce or close a current
+              position so that your position size will not be increased
+              unintentionally.
+            </span>
+          </div>
+          <TooltipArrow className="orderly-fill-base-400" />
+        </TooltipContent>
+      </Tooltip>
+    </>
+  );
 };
