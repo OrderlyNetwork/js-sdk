7eaafab79d9a7a602396bbafdb054d2e49f1e4c8
Mark <mark@orderly.network>
2024-08-22 15:17:24 +0800

    treading rewards add hints
    issue-1332

--- packages/hooks/src/trading-rewards/index.ts
+++ packages/hooks/src/trading-rewards/index.ts
@@ -2,9 +2,13 @@ export { useEpochInfo } from "./useEpochInfo";
 export type { EpochInfoType, EpochInfoItem } from "./useEpochInfo";
 
 export { useAllBrokers } from "./useAllBrokers";
+export type { Brokers } from "./useAllBrokers";
 
 export { useCurEpochEstimate } from "./useCurEpochEstimate";
 export type { CurrentEpochEstimate } from "./useCurEpochEstimate";
+
+export type { AccountRewardsHistoryRow, AccountRewardsHistory } from "./useAccountRewardHistory";
+export { useAccountRewardsHistory } from "./useAccountRewardHistory";
 
 export { useGetClaimed, DistributionId } from "./useGetClaimed";
 
