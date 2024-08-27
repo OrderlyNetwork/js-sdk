dd056ca21fc114b46392aa9259015a3426e83f70
Leo <leo@orderly.network>
2024-08-27 10:55:48 +0800

    update position lib

--- packages/ui-positions/src/components/priceInput.tsx
+++ packages/ui-positions/src/components/priceInput.tsx
@@ -1,15 +1,42 @@
-import { ArrowDownUpIcon, CaretDownIcon, Input } from "@orderly.network/ui";
+import {
+  CaretDownIcon,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuRoot,
+  DropdownMenuTrigger,
+  Input,
+} from "@orderly.network/ui";
 
 export const PriceInput = () => {
   return (
-    <Input
-      size="sm"
-      value={"Market"}
-      suffix={
-        <button className="oui-px-1">
-          <CaretDownIcon size={12} color="white" />
-        </button>
-      }
-    />
+    <DropdownMenuRoot>
+      <Input
+        size="sm"
+        value={"Market"}
+        suffix={
+          <DropdownMenuTrigger asChild>
+            <button className="oui-px-1 oui-h-full">
+              <CaretDownIcon size={12} color="white" />
+            </button>
+          </DropdownMenuTrigger>
+        }
+      />
+      <DropdownMenuContent
+        align="end"
+        className="oui-w-[96px] oui-min-w-[96px]"
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            size="xs"
+            onSelect={(vent) => {
+              console.log(vent);
+            }}
+          >
+            <span>Market</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenuRoot>
   );
 };
