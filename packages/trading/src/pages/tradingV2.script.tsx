563ba5659396056ed0430f650be405349e0595b0
Mark <mark@orderly.network>
2024-09-24 15:47:58 +0800

    update script

--- packages/trading/src/pages/tradingV2.script.tsx
+++ packages/trading/src/pages/tradingV2.script.tsx
@@ -1,14 +1,9 @@
-import { useMediaQuery } from "@orderly.network/hooks";
 import { useTradingPateContext } from "../provider/context";
 import { TradingPageV2Props } from "../types/types";
-import { MEDIA_TABLET } from "@orderly.network/types";
 
 export const useTradingV2Script = () => {
     const props = useTradingPateContext();
-    return {
-        ...props,
-        isMobileLayout,
-    };
+    return props;
 };
 
 export type TradingV2State = ReturnType<typeof useTradingV2Script>;
