9566739ada10b131acebc716925493be9ca2db3e
Mark <mark@orderly.network>
2024-10-11 17:48:24 +0800

    add bottom navigation bar and modify mobile layout

---
+++ packages/trading/src/components/mWeb/bottomNavBar/bottomNavBar.ui.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { Flex, Text } from "@orderly.network/ui";
+import { BottomNavBarState } from "./bottomNavBar.script";
+
+
+export const BottomNavBar: FC<BottomNavBarState> = (props) => {
+
+    return (
+        <Flex height={44}></Flex>
+    );
+}
