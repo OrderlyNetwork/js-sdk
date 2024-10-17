9f7bc4c8bf1db103976df9ab5f8d2c8c1bde3447
Mark <mark@orderly.network>
2024-10-17 10:17:17 +0800

    update edit button

--- packages/ui-orders/src/components/orderList/mWeb/editBtn/editBtn.script.tsx
+++ packages/ui-orders/src/components/orderList/mWeb/editBtn/editBtn.script.tsx
@@ -3,22 +3,27 @@ import { OrderCellState } from "../orderCell.script";
 import { EditSheetWidget } from "../editSheet";
 import { useCallback } from "react";
 import { useOrderListContext } from "../../orderListContext";
+import { TabType } from "../../../orders.widget";
 
 export const useEditBtnScript = (props: { state: OrderCellState }) => {
   const { state } = props;
   const { editAlgoOrder, editOrder } = useOrderListContext();
   const onShowEditSheet = useCallback(() => {
-    modal.dialog({
-      title: "Edit order",
-      size: "xs",
-      content: (
-        <EditSheetWidget
-          state={state}
-          editAlgoOrder={editAlgoOrder}
-          editOrder={editOrder}
-        />
-      ),
-    });
+    if (props.state.type === TabType.tp_sl) {
+      // TODO: show tp/sl edit
+    } else {
+      modal.sheet({
+        title: "Edit order",
+        // size: "xs",
+        content: (
+          <EditSheetWidget
+            state={state}
+            editAlgoOrder={editAlgoOrder}
+            editOrder={editOrder}
+          />
+        ),
+      });
+    }
   }, [state]);
 
   return {
