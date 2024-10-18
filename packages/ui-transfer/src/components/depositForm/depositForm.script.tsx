3c35a726992f51ab55c11d88f3effcbb8d84761d
tony <tony@woo.network>
2024-10-18 11:00:13 +0800

    fix decimal

--- packages/ui-transfer/src/components/depositForm/depositForm.script.tsx
+++ packages/ui-transfer/src/components/depositForm/depositForm.script.tsx
@@ -152,6 +152,7 @@ export function useDepositFee(options: {
 
   const feeProps = useMemo(() => {
     const dstGasFee = new Decimal(depositFee.toString())
+        // todo solana is 9, evm is 18
       .div(new Decimal(10).pow(9))
       .toString();
 
