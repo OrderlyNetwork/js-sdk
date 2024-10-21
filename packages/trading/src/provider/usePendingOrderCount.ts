5380ce208429fa5cbedcfa79ded39c2d2a33667f
Jame Zhan <jame@orderly.network>
2024-10-21 11:52:19 +0800

    remove trading public assets

--- packages/trading/src/provider/usePendingOrderCount.ts
+++ packages/trading/src/provider/usePendingOrderCount.ts
@@ -1,13 +1,13 @@
 import { useOrderStream } from "@orderly.network/hooks";
 import { useTradingLocalStorage } from "./useTradingLocalStorage";
 import { AlgoOrderRootType, OrderStatus } from "@orderly.network/types";
+import { useMemo } from "react";
 
 export const usePendingOrderCount = (symbol?: string) => {
   const { showAllSymbol } = useTradingLocalStorage();
 
   const [_, { total: pendingCount }] = useOrderStream(
     {
-      symbol: showAllSymbol ? undefined: symbol,
       status: OrderStatus.INCOMPLETE,
       excludes: [AlgoOrderRootType.POSITIONAL_TP_SL, AlgoOrderRootType.TP_SL],
     },
@@ -18,7 +18,6 @@ export const usePendingOrderCount = (symbol?: string) => {
 
   const [__, { total: tpslCount }] = useOrderStream(
     {
-      symbol: showAllSymbol ? undefined: symbol,
       status: OrderStatus.INCOMPLETE,
       includes: [AlgoOrderRootType.POSITIONAL_TP_SL, AlgoOrderRootType.TP_SL],
     },
