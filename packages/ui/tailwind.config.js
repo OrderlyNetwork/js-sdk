62120164792bfd72ef2ed71c9f65e2137a6a5008
Jame Zhan <jame@orderly.network>
2024-10-22 12:32:29 +0800

    update ui tailwind config

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -28,6 +28,7 @@ module.exports = withTV({
     "../chart/src/**/*.{ts,js,tsx,jsx}",
     "../ui-transfer/src/**/*.{ts,js,tsx,jsx}",
     "../trading/src/**/*.{ts,js,tsx,jsx}",
+    "../ui-order-entry/src/**/*.{ts,js,tsx,jsx}",
   ],
   prefix: "oui-",
   theme: {
