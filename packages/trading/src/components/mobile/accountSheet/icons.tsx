8977968ddd4227276a3b3b1d912a4eee80aa6f8e
Mark <mark@orderly.network>
2024-10-30 15:56:21 +0800

    edit order & listen WS msg show toast & orderEntry & edit order sheet

--- packages/trading/src/components/mobile/accountSheet/icons.tsx
+++ packages/trading/src/components/mobile/accountSheet/icons.tsx
@@ -6,6 +6,7 @@ export const HeadIcon = () => {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="oui-rounded-full"
     >
       <g clipPath="url(#a)">
         <path
