3c34fe4d42f45399610ae2da857e419ab884ff59
Jame Zhan <jame@orderly.network>
2024-11-13 21:50:25 +0800

    update orderly codemod cli

--- packages/cli-codemod/bin/cli.js
+++ packages/cli-codemod/bin/cli.js
@@ -2,64 +2,99 @@
 
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 
-const argv = yargs(hideBin(process.argv))
-  .scriptName("orderly-codemod")
-  .usage("Scan and collect props from OrderlyAppProvider components")
-  .usage("Usage: $0 [OPTION] PATH")
-  .options({
-    parser: {
-      alias: "p",
-      type: "string",
-      default: "tsx",
-      choices: ["babel", "babylon", "flow", "ts", "tsx"],
-      describe: "the parser to use for parsing the source files",
-    },
-    extensions: {
-      alias: "ext",
-      type: "string",
-      default: "tsx",
-      describe:
-        "transform files with these file extensions (comma separated list)",
-    },
-    ignore: {
-      alias: "i",
-      type: "string",
-      default: "node_modules",
-      describe: "ignore files that match a provided glob expression",
-    },
-  })
-  .demandCommand(
-    1,
-    "Error: You have to provide at least one file/directory to transform."
-  )
-  .help()
-  .alias("help", "h").argv;
+main();
 
-const { _, parser, extensions, ignore } = argv;
+function main() {
+  const argv = getArgv();
 
-const path = _[0];
-
-const i =
-  ignore === "node_modules"
-    ? ignore
-    : `node_modules --ignore-pattern=${ignore}`;
-
-const command = `npx jscodeshift --parser=${parser} --extensions=${extensions} --ignore-pattern=${i} --transform=./command/transformer.js ${path}`;
-
-// console.log("command: ", command);
-
-// TODO: eslint
-
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing command: ${error.message}`);
+  if (hasUncommittedChanges()) {
+    console.error("Error: There are uncommitted changes in the repository.");
     return;
   }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
+
+  const command = getCommand(argv);
+
+  execCommand(command);
+}
+
+function getArgv() {
+  const argv = yargs(hideBin(process.argv))
+    .scriptName("orderly-codemod")
+    .usage("Scan and collect props from OrderlyAppProvider components")
+    .usage("Usage: $0 [OPTION] PATH")
+    .options({
+      parser: {
+        alias: "p",
+        type: "string",
+        default: "tsx",
+        choices: ["babel", "babylon", "flow", "ts", "tsx"],
+        describe: "the parser to use for parsing the source files",
+      },
+      extensions: {
+        alias: "ext",
+        type: "string",
+        default: "tsx",
+        describe:
+          "transform files with these file extensions (comma separated list)",
+      },
+      ignore: {
+        alias: "i",
+        type: "string",
+        default: "node_modules",
+        describe: "ignore files that match a provided glob expression",
+      },
+    })
+    .demandCommand(
+      1,
+      "Error: You have to provide at least one file/directory to transform."
+    )
+    .help()
+    .alias("help", "h").argv;
+
+  return argv;
+}
+
+function getCommand(argv) {
+  const { _, parser, extensions, ignore } = argv;
+
+  const _path = _[0];
+
+  const i =
+    ignore === "node_modules"
+      ? ignore
+      : `node_modules --ignore-pattern=${ignore}`;
+
+  const command = `npx jscodeshift --parser=${parser} --extensions=${extensions} --ignore-pattern=${i} --transform=./command/transformer.js ${_path}`;
+
+  // console.log("command: ", command);
+  return command;
+}
+
+function execCommand(command) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`error: ${stderr}`);
+      return;
+    }
+    console.log(`${stdout}`);
+  });
+}
+
+function hasUncommittedChanges() {
+  try {
+    const statusOutput = execSync("git status --porcelain", {
+      encoding: "utf-8",
+    });
+
+    return statusOutput.trim().length > 0;
+  } catch (error) {
+    console.error("Error checking git status:", error);
+    return false;
   }
-  console.log(`stdout: ${stdout}`);
-});
+}
