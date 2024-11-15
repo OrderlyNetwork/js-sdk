e95fca582266f0f8e5e8252311bc4618459985a7
Jame Zhan <jame@orderly.network>
2024-11-15 15:46:19 +0800

    remove @orderly.network/react import

--- apps/storybook2/vite.config.ts
+++ apps/storybook2/vite.config.ts
@@ -120,11 +120,6 @@ export default defineConfig({
         __dirname,
         "../../packages/component/dist"
       ),
-      "@orderly.network/react": resolve(
-        __dirname,
-        "../../packages/component/src"
-      ),
-      "@": resolve(__dirname, "../../packages/component/src"),
     },
   },
 });
