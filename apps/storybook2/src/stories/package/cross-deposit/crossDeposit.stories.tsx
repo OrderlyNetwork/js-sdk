ed1d2a8e3feebbe757e2614dad88a9268a3b0d38
Jame Zhan <jame@orderly.network>
2024-11-18 15:56:58 +0800

    update cross deposit stories

--- apps/storybook2/src/stories/package/cross-deposit/crossDeposit.stories.tsx
+++ apps/storybook2/src/stories/package/cross-deposit/crossDeposit.stories.tsx
@@ -1,85 +1,65 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { OrderlyApp } from "@orderly.network/react-app";
-import { ConnectorProvider } from "@orderly.network/web3-onboard";
-import { CrossDepositFormWidget, installCrossDeposit } from '@orderly.network/ui-cross-deposit';
+import {
+  CrossDepositFormWidget,
+  installCrossDeposit,
+} from "@orderly.network/ui-cross-deposit";
 import { Box, Flex, Button, modal } from "@orderly.network/ui";
-import { CustomConfigStore } from "../CustomConfigStore";
-import { customChains } from "./customChains";
-import { DepositAndWithdrawWithDialogId, DepositAndWithdrawWithSheetId } from "@orderly.network/ui-transfer";
-installCrossDeposit()
+import {
+  DepositAndWithdrawWithDialogId,
+  DepositAndWithdrawWithSheetId,
+} from "@orderly.network/ui-transfer";
+installCrossDeposit();
 
-const networkId = "mainnet";
-// const networkId = "testnet";
-const configStore = new CustomConfigStore({ networkId, env: "prod", brokerName: 'Woofi Pro', brokerId: 'woofi_pro' });
-
-
-const meta = {
-    title: "Package/ui-cross-deposit",
-    component: CrossDepositFormWidget,
-    subcomponents: {
-
-    },
-    decorators: [
-        (Story: any) => (
-            <ConnectorProvider>
-                <OrderlyApp
-                    // brokerId="orderly"
-                    // brokerName="Orderly"
-                    networkId={networkId}
-                    configStore={configStore}
-                    appIcons={{
-                        main: {
-                            img: "/orderly-logo.svg",
-                        },
-                        secondary: {
-                            img: "/orderly-logo-secondary.svg",
-                        },
-                }}
-                customChains={customChains as any}>
-                    <Story />
-                </OrderlyApp>
-            </ConnectorProvider>
-        ),
-    ],
-} satisfies Meta<typeof CrossDepositFormWidget>;
+const meta: Meta<typeof CrossDepositFormWidget> = {
+  title: "Package/ui-cross-deposit",
+  component: CrossDepositFormWidget,
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const CrossDepositForm: Story = {
-    decorators: [
-        (Story) => (
-            <Flex justify='center' mt={10}>
-                <Box width={420} intensity={800} p={5} r="lg">
-                    <Story />
-                </Box>
-            </Flex>
-        ),
-    ],
+  decorators: [
+    (Story) => (
+      <Flex justify="center" mt={10}>
+        <Box width={420} intensity={800} p={5} r="lg">
+          <Story />
+        </Box>
+      </Flex>
+    ),
+  ],
 };
 
 export const CrossDepositDialog: Story = {
-    decorators: [
-        (Story) => (
-            <Flex justify='center' itemAlign='center' height="100vh">
-                <Button onClick={() => {
-                    modal.show(DepositAndWithdrawWithDialogId, { activeTab: 'deposit' })
-
-                }}>Show Deposit Dialog</Button>
-            </Flex>
-        ),
-    ],
+  decorators: [
+    (Story) => (
+      <Flex justify="center" itemAlign="center" height="100vh">
+        <Button
+          onClick={() => {
+            modal.show(DepositAndWithdrawWithDialogId, {
+              activeTab: "deposit",
+            });
+          }}
+        >
+          Show Deposit Dialog
+        </Button>
+      </Flex>
+    ),
+  ],
 };
 
 export const CrossDepositSheet: Story = {
-    decorators: [
-        (Story) => (
-            <Flex justify='center' itemAlign='center' height="100vh">
-                <Button onClick={() => {
-                    modal.show(DepositAndWithdrawWithSheetId, { activeTab: 'deposit' })
-
-                }}>Show Deposit Sheet</Button>
-            </Flex>
-        ),
-    ],
+  decorators: [
+    (Story) => (
+      <Flex justify="center" itemAlign="center" height="100vh">
+        <Button
+          onClick={() => {
+            modal.show(DepositAndWithdrawWithSheetId, { activeTab: "deposit" });
+          }}
+        >
+          Show Deposit Sheet
+        </Button>
+      </Flex>
+    ),
+  ],
 };
