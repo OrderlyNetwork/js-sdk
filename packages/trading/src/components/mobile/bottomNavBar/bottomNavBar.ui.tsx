46001e5b75f1d4ebca1b49bd4a4f5953eef6fef4
Mark <mark@orderly.network>
2024-11-19 23:22:04 +0800

    modify item-align to adapter mobile bottom nav bar

--- packages/trading/src/components/mobile/bottomNavBar/bottomNavBar.ui.tsx
+++ packages/trading/src/components/mobile/bottomNavBar/bottomNavBar.ui.tsx
@@ -20,6 +20,7 @@ export const BottomNavBar: FC<BottomNavBarState> = (props) => {
       // height={64}
       gap={1}
       justify={"between"}
+      itemAlign={"start"}
       className="oui-bg-base-9 oui-px-[14px] oui-pt-[7px] oui-border-t oui-border-line-4"
       style={{
         height: "calc(64px + env(safe-area-inset-bottom))"
