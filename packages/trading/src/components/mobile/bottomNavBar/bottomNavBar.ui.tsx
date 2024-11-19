0dd89c1356adbfde9593073152416b55110bfcb6
Mark <mark@orderly.network>
2024-11-19 22:51:02 +0800

    adapter mobile bottom nav bar

--- packages/trading/src/components/mobile/bottomNavBar/bottomNavBar.ui.tsx
+++ packages/trading/src/components/mobile/bottomNavBar/bottomNavBar.ui.tsx
@@ -17,10 +17,13 @@ import { BalanceWidget } from "./balance";
 export const BottomNavBar: FC<BottomNavBarState> = (props) => {
   return (
     <Flex
-      height={64}
+      // height={64}
       gap={1}
       justify={"between"}
       className="oui-bg-base-9 oui-px-[14px] oui-pt-[7px] oui-border-t oui-border-line-4"
+      style={{
+        height: "calc(64px + env(safe-area-inset-bottom))"
+      }}
     >
       <BalanceWidget />
 
