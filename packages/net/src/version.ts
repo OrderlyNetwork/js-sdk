ab0a67fa56b741466920177140d5b82314ae2e07
Mark <mark@orderly.network>
2024-11-26 11:04:50 +0800

    publish

--- packages/net/src/version.ts
+++ packages/net/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 }
 if(typeof window !== 'undefined') {
     window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-    window.__ORDERLY_VERSION__["@orderly.network/net"] = "2.0.1-preReleaseV2.2";
+    window.__ORDERLY_VERSION__["@orderly.network/net"] = "2.0.1-preReleaseV2.3";
 };
 
-export default "2.0.1-preReleaseV2.2";
+export default "2.0.1-preReleaseV2.3";
