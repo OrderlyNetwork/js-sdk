a4fec8c0da634e64f4dabdc2b98126d944169bfe
Jame Zhan <jame@orderly.network>
2025-01-14 09:05:52 +0800

    publish

--- packages/cli-codemod/CHANGELOG.md
+++ packages/cli-codemod/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @orderly.network/codemod
 
+## 1.0.2-internal-20250116.2
+
+### Patch Changes
+
+- publish
+
 ## 1.0.2-internal-20250116.1
 
 ### Patch Changes
--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 }
 if(typeof window !== 'undefined') {
     window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-    window.__ORDERLY_VERSION__["@orderly.network/core"] = "2.0.2-internal-20250116.1";
+    window.__ORDERLY_VERSION__["@orderly.network/core"] = "2.0.2-internal-20250116.2";
 };
 
-export default "2.0.2-internal-20250116.1";
+export default "2.0.2-internal-20250116.2";
