b59f31e18854659e4caa05a0159eb5a569b2cd46
tony <tony@woo.network>
2025-02-13 17:28:19 +0800

    ui

--- packages/wallet-connector-privy/src/components/connectDrawer.tsx
+++ packages/wallet-connector-privy/src/components/connectDrawer.tsx
@@ -1,4 +1,4 @@
-import { SimpleDialog, } from "@orderly.network/ui";
+import { SimpleDialog, useScreen, } from "@orderly.network/ui";
 import React, { useMemo } from "react";
 import { useWallet } from "../useWallet";
 import { usePrivyWallet } from "../usePrivyWallet";
@@ -13,12 +13,40 @@ import { useLocalStorage } from "@orderly.network/hooks";
 import { RenderNoPrivyWallet } from "./renderNoPrivyWallet";
 import { CloseIcon } from "./icons";
 import { WalletAdapter } from "@solana/wallet-adapter-base";
+import { cn } from "@orderly.network/ui";
+import { Connector } from "wagmi";
+
+// 添加一个wallet icon映射
+const WALLET_ICONS: Record<string, string> = {
+  'injected': 'https://oss.orderly.network/static/wallet_icon/metamask.png',
+  'walletConnect': 'https://oss.orderly.network/static/wallet_icon/walletconnect.png',
+  'coinbaseWallet': 'https://oss.orderly.network/static/wallet_icon/coinbase.png',
+};
 
 function PrivyConnectArea({ connect }: { connect: (type: any) => void }) {
+  const { isMobile, isDesktop } = useScreen();
   return (
     <div className="">
-      <div className="oui-text-base-contrast-80 oui-text-sm oui-font-semibold oui-mb-2">Login in</div>
-      <div className="oui-flex oui-flex-col oui-gap-[6px]">
+      <div className={cn(
+        "oui-flex oui-items-center oui-justify-between",
+        "oui-text-base-contrast-80 oui-text-sm oui-font-semibold oui-mb-3",
+        "md:oui-mb-2"
+      )}>Login in
+        {isMobile &&
+          <div className="oui-h-3 oui-flex oui-justify-center">
+
+            <img src="https://oss.orderly.network/static/sdk/privy-logo.png" className=" oui-h-[10px]" />
+          </div>
+        }
+
+
+      </div>
+      <div className={cn(
+
+        "oui-grid oui-grid-cols-2 oui-gap-[6px]",
+        "md:oui-flex md:oui-flex-col md:oui-gap-[6px]",
+
+      )}>
 
         <div
 
@@ -46,17 +74,38 @@ function PrivyConnectArea({ connect }: { connect: (type: any) => void }) {
         </div>
 
       </div>
-      <div className="oui-h-3 oui-flex oui-justify-center oui-mt-4">
+      {isDesktop &&
+        <div className="oui-h-3 oui-flex oui-justify-center oui-mt-4">
 
-        <img src="https://oss.orderly.network/static/sdk/privy-logo.png" className=" oui-h-[10px]" />
-      </div>
-      <div className="oui-h-[1px] oui-bg-line oui-w-full oui-mt-5"></div>
+          <img src="https://oss.orderly.network/static/sdk/privy-logo.png" className=" oui-h-[10px]" />
+        </div>
+      }
+      <div className="oui-h-[1px] oui-bg-line oui-w-full oui-mt-4 md:oui-mt-5"></div>
     </div>
   )
 }
 
 function EVMConnectArea({ connect }: { connect: (type: any) => void }) {
   const { connectors } = useWagmiWallet();
+
+  const getWalletIcon = (connector: Connector): string | undefined => {
+    // inject wallet icon
+    if (connector.id === 'injected') {
+      // 检查是否是MetaMask
+      if (typeof window !== 'undefined' && window.ethereum?.isMetaMask) {
+        return 'https://oss.orderly.network/static/wallet_icon/metamask.png';
+      }
+      // 检查是否是Coinbase Wallet
+      if (typeof window !== 'undefined' && window.ethereum?.isCoinbaseWallet) {
+        return 'https://oss.orderly.network/static/wallet_icon/coinbase.png';
+      }
+      // 其他注入钱包
+      return 'https://oss.orderly.network/static/wallet_icon/default.png';
+    }
+
+    return WALLET_ICONS[connector.id]
+  };
+
   return (
     <div className="">
       <div className="oui-text-base-contrast-80 oui-text-sm oui-font-semibold oui-mb-2">EVM</div>
@@ -66,28 +115,36 @@ function EVMConnectArea({ connect }: { connect: (type: any) => void }) {
             className=" oui-flex oui-items-center oui-justify-start oui-gap-1 oui-rounded-[6px] oui-px-2 oui-bg-[#07080A] oui-py-[11px] oui-flex-1 oui-cursor-pointer"
             onClick={() => connect(item)}
           >
-            <img className="oui-w-[18px] oui-h-[18px]" src='https://oss.orderly.network/static/wallet_icon/metamask.png' />
+            {
+              getWalletIcon(item) &&
+
+              <img
+                className="oui-w-[18px] oui-h-[18px]"
+                src={getWalletIcon(item)}
+                alt={item.name}
+              />
+            }
             <div className="oui-text-base-contrast oui-text-2xs">{item.name}</div>
           </div>
         ))}
-
       </div>
     </div>
   )
-
 }
 
-function SOLConnectArea({ connect }: { connect: (walletAdapter:WalletAdapter) => void }) {
+function SOLConnectArea({ connect }: { connect: (walletAdapter: WalletAdapter) => void }) {
   const { wallets } = useSolanaWallet();
+
   return (
     <div>
       <div className="oui-text-base-contrast-80 oui-text-sm oui-font-semibold oui-mb-2">SOL</div>
       <div className="oui-grid oui-grid-cols-2 oui-gap-[6px]">
         {wallets.map((item, key) => (
           <div key={key}
-            className=" oui-flex oui-items-center oui-justify-center oui-rounded-[6px] oui-px-2 oui-bg-[#07080A] oui-py-[11px] oui-flex-1 oui-cursor-pointer"
+            className=" oui-flex oui-items-center oui-justify-center oui-gap-1 oui-rounded-[6px] oui-px-2 oui-bg-[#07080A] oui-py-[11px] oui-flex-1 oui-cursor-pointer"
             onClick={() => connect(item.adapter)}
           >
+            {item.adapter.icon && <img src={item.adapter.icon} className="oui-w-[18px] oui-h-[18px]" />}
             <div className="oui-text-base-contrast oui-text-2xs">{item.adapter.name}</div>
           </div>
         ))}
@@ -109,7 +166,10 @@ function ConnectWallet() {
   };
 
   return (
-    <div className="oui-flex oui-flex-col oui-gap-5">
+    <div className={cn(
+      "oui-flex oui-flex-col oui-gap-4",
+      "md:oui-gap-5"
+    )}>
       <PrivyConnectArea connect={(type) => handleConnect({ walletType: WalletType.PRIVY, extraType: type })} />
       <EVMConnectArea connect={(connector) => handleConnect({ walletType: WalletType.EVM, connector: connector })} />
       <SOLConnectArea connect={(walletAdapter) => handleConnect({ walletType: WalletType.SOL, walletAdapter: walletAdapter })} />
@@ -165,7 +225,7 @@ function MyWallet() {
 export function ConnectDrawer(props: { open: boolean, onChangeOpen: (open: boolean) => void }) {
   const { isConnected: isConnectedPrivy } =
     usePrivyWallet();
-  const {  isConnected: isConnectedEvm } = useWagmiWallet();
+  const { isConnected: isConnectedEvm } = useWagmiWallet();
   const { isConnected: isConnectedSolana } = useSolanaWallet();
 
   const isConnected = useMemo(() => {
@@ -181,11 +241,21 @@ export function ConnectDrawer(props: { open: boolean, onChangeOpen: (open: boole
     return false;
   }, [isConnectedPrivy, isConnectedEvm, isConnectedSolana])
 
+  const { isMobile } = useScreen();
+
   return (
     <SimpleDialog
       classNames={{
-        content: 'oui-flex-col  oui-h-[calc(100vh_-_72px)] oui-right-0 oui-left-[calc(100%_-_300px)] oui-w-[300px] oui-translate-x-0 lg:oui-px-4',
-        body: 'oui-overflow-hidden oui-h-full oui-relative',
+        content: cn(
+          'oui-flex-col oui-h-screen  oui-right-0 oui-left-[calc(100%_-_276px)]  oui-w-[276px] oui-translate-x-0 oui-px-4 oui-rounded-0',
+          'md:oui-h-[calc(100vh_-_72px)] md:oui-left-[calc(100%_-_300px)] md:oui-w-[300px] ',
+        ),
+        body: cn(
+          'oui-overflow-hidden oui-h-full oui-relative oui-flex oui-flex-col oui-justify-between',
+          'oui-rounded-0 oui-pb-6 oui-pt-0',
+          'md:oui-py-4'
+
+        ),
       }}
       open={props.open}
       onOpenChange={props.onChangeOpen}
@@ -194,15 +264,21 @@ export function ConnectDrawer(props: { open: boolean, onChangeOpen: (open: boole
         // onPointerDownOutside: (event) => event.preventDefault(),
       }}
     >
-      <div className='oui-z-0 oui-absolute -oui-top-[calc(100vh/2)] oui-h-[100vh] oui-left-[50px] oui-right-[50px]'
-        style={{
-          background: "conic-gradient(from -41deg at 40.63% 50.41%, rgba(242, 98, 181, 0.00) 125.17920970916748deg, rgba(95, 197, 255, 0.20) 193.4119462966919deg, rgba(255, 172, 137, 0.20) 216.0206937789917deg, rgba(129, 85, 255, 0.20) 236.0708713531494deg, rgba(120, 157, 255, 0.20) 259.95326042175293deg, rgba(159, 115, 241, 0.00) 311.0780096054077deg)",
-          filter: "blur(50px)",
-        }}
-      />
-      <div className="oui-z-10 oui-relative oui-h-full">
-        <div className="oui-flex oui-justify-between oui-items-center oui-mb-5">
-          <div className='oui-font-semibold oui-text-base-contrast-80 oui-text-base'>{isConnected ? 'My Wallet' : 'Connect Wallet'}</div>
+      {!isMobile &&
+        <div className='oui-z-0 oui-absolute -oui-top-[calc(100vh/2)] oui-h-[100vh] oui-left-[50px] oui-right-[50px]'
+          style={{
+            background: "conic-gradient(from -41deg at 40.63% 50.41%, rgba(242, 98, 181, 0.00) 125.17920970916748deg, rgba(95, 197, 255, 0.20) 193.4119462966919deg, rgba(255, 172, 137, 0.20) 216.0206937789917deg, rgba(129, 85, 255, 0.20) 236.0708713531494deg, rgba(120, 157, 255, 0.20) 259.95326042175293deg, rgba(159, 115, 241, 0.00) 311.0780096054077deg)",
+            filter: "blur(50px)",
+          }}
+        />
+      }
+      <div className="oui-z-10 oui-relative">
+        <div className="oui-flex oui-justify-between oui-items-center oui-mb-4 md:oui-mb-5">
+          <div className={cn(
+            'oui-font-semibold oui-text-base-contrast-80 ',
+            'oui-text-[20px] oui-py-2',
+            'md:oui-text-base md:oui-py-0',
+          )}>{isConnected ? 'My Wallet' : 'Connect Wallet'}</div>
           <CloseIcon className="oui-cursor-pointer oui-text-base-contrast-20 oui-w-5 oui-h-5 hover:oui-text-base-contrast-80" onClick={() => props.onChangeOpen(false)} />
         </div>
         {isConnected ? (
@@ -214,7 +290,7 @@ export function ConnectDrawer(props: { open: boolean, onChangeOpen: (open: boole
 
       {
         !isConnected &&
-        <div className="oui-z-10 oui-text-base-contrast-80 oui-text-center oui-text-2xs oui-absolute oui-bottom-0 oui-left-0 oui-right-0 oui-px-4 oui-pb-4  oui-font-semibold">
+        <div className="oui-z-10 oui-text-base-contrast-80 oui-text-center oui-text-2xs oui-relative  oui-font-semibold">
           By connecting your wallet, you acknowledge and agree to the <span className="oui-cursor-pointer oui-underline oui-text-primary">terms of use</span>.
         </div>
       }
