716b4dfc64841faad6ce5b31d460957a3c28c0a1
Jame Zhan <jame@orderly.network>
2025-03-10 00:06:12 +0800

    add share locale

---
+++ packages/i18n/src/locale/module/share.ts
@@ -0,0 +1,34 @@
+export const share = {
+  "share.pnl.share": "Share",
+  "share.pnl.sharePnl": "Share PnL",
+  
+  "share.pnl.share.long": "LONG",
+  "share.pnl.share.short": "SHORT",
+
+  "share.pnl.displayFormat": "PnL display format",
+  "share.pnl.displayFormat.roiPnl": "ROI & PnL",
+  "share.pnl.displayFormat.roi": "ROI",
+  "share.pnl.displayFormat.pnl": "PnL",
+
+  "share.pnl.optionalInfo": "Optional information to share",
+  "share.pnl.optionalInfo.openPrice": "Open price",
+  "share.pnl.optionalInfo.closePrice": "Close price",
+  "share.pnl.optionalInfo.openTime": "Opened at",
+  "share.pnl.optionalInfo.closeTime": "Closed at",
+  "share.pnl.optionalInfo.markPrice": "Mark price",
+  "share.pnl.optionalInfo.quantity": "Quantity",
+  "share.pnl.optionalInfo.leverage": "Leverage",
+  "share.pnl.optionalInfo.message": "Your message",
+  "share.pnl.optionalInfo.message.placeholder": "Max 25 characters",
+  "share.pnl.optionalInfo.message.maxLength":
+    "Maximum support of 25 characters",
+
+  "share.pnl.download": "Download",
+  "share.pnl.copy": "Copy",
+  "share.pnl.copySuccess": "Image copied",
+  "share.pnl.copyFailed": "Copy failed",
+  "share.pnl.copyFailed.description":
+    "Browser version outdated, please update in order to copy image to clipboard.",
+};
+
+export type Share = typeof share;
