73ff8f23567d49fe1547ad4b8afb5b5ce15c26d2
Jame Zhan <jame@orderly.network>
2025-03-11 19:57:04 +0800

    update positions and orders stories

--- apps/storybook/src/stories/package/portfolio/positions.stories.tsx
+++ apps/storybook/src/stories/package/portfolio/positions.stories.tsx
@@ -66,7 +66,9 @@ export const LayoutPage: Story = {
     return (
       <PortfolioLayout currentPath={PortfolioLeftSidebarPath.Positions}>
         <Container>
-          <PositionsModule.PositionsPage />
+          <PositionsModule.PositionsPage
+            sharePnLConfig={config.tradingPage.sharePnLConfig}
+          />
         </Container>
       </PortfolioLayout>
     );
