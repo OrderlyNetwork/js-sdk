94f8bcb7e079495fb6cc0a64f8f615992da5af65
Jame Zhan <jame@orderly.network>
2025-03-27 12:26:58 +0800

    let main nav menus show spacing when overlap

--- packages/ui-scaffold/src/components/main/mainNav.ui.tsx
+++ packages/ui-scaffold/src/components/main/mainNav.ui.tsx
@@ -1,7 +1,7 @@
 import { FC, PropsWithChildren, useMemo } from "react";
 import { MainNavClassNames, MainNavItemsProps } from "./mainNavItems";
 import { ProductsMenu, ProductsProps } from "./products";
-import { cn, Divider, Flex } from "@orderly.network/ui";
+import { Box, cn, Divider, Flex } from "@orderly.network/ui";
 import type { LogoProps } from "@orderly.network/ui";
 import { AccountSummaryWidget } from "../accountSummary";
 import { ChainMenuWidget } from "../chainMenu";
@@ -67,13 +67,21 @@ export const MainNav: FC<PropsWithChildren<MainNavProps>> = (props) => {
       height={"48px"}
       justify={"between"}
       px={3}
+      gapX={3}
       className={cn(
         "oui-main-nav oui-font-semibold",
         className,
         classNames?.root
       )}
     >
-      <Flex itemAlign={"center"} className="oui-gap-3 2xl:oui-gap-4">
+      <Flex
+        itemAlign={"center"}
+        className={cn(
+          "oui-gap-3 2xl:oui-gap-4",
+          // let the left and right views show spacing when overlapping
+          "oui-overflow-hidden"
+        )}
+      >
         <MainLogo {...logo} />
         <ProductsMenu {...products} className={classNames?.products} />
         {/* <MainNavItems {...props.mainMenus} classNames={classNames?.mainNav} /> */}
