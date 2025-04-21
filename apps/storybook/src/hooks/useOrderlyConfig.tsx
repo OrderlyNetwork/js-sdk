60db8e0b53c0ae452e0a1b6aefeb34466a79bcb0
Jame Zhan <jame@orderly.network>
2025-04-21 11:48:01 +0800

    update extends locales

--- apps/storybook/src/hooks/useOrderlyConfig.tsx
+++ apps/storybook/src/hooks/useOrderlyConfig.tsx
@@ -11,7 +11,7 @@ import {
   TradingRewardsActiveIcon,
   TradingRewardsIcon,
 } from "../components/icons";
-import { Trans, useTranslation } from "@orderly.network/i18n";
+import { useTranslation } from "@orderly.network/i18n";
 
 export type OrderlyConfig = {
   orderlyAppProvider: {
@@ -97,32 +97,8 @@ export const useOrderlyConfig = () => {
           enableDefault: true,
           customRestrictedIps: [],
           customRestrictedRegions: [],
-          content: ({ ip, brokerName }) => {
-            const email = "x@orerly.network";
-            return (
-              // @ts-ignore
-              <Trans
-                // @ts-ignore
-                i18nKey="extend.restrictedInfo.description"
-                values={{ ip, brokerName, email }}
-                components={[
-                  <a
-                    style={{
-                      textDecorationLine: "underline",
-                    }}
-                    target="_blank"
-                    href="https://learn.woo.org/legal/terms-of-use#id-2.-legal-compliance"
-                  />,
-                  <a
-                    style={{
-                      textDecorationLine: "underline",
-                    }}
-                    href={`mailto:${email}`}
-                  />,
-                ]}
-              />
-            );
-          },
+          // content: ({ ip, brokerName }) =>
+          //   `You are accessing ${brokerName} from an IP address (${ip}) associated with a restricted country. Please refer to our Terms of Use</0>. If you believe this is an error, contact x@orerly.network.`,
         },
       },
       tradingPage: {
