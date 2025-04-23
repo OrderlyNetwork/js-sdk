3eb680520e1a654889add2fd18b20e21038d320b
Leo <leo@orderly.network>
2025-04-23 16:29:05 +0800

    Add additional info management to Account class with LocalStorage support

--- packages/core/src/account.ts
+++ packages/core/src/account.ts
@@ -18,6 +18,8 @@ import { WalletAdapter } from "./wallet/walletAdapter";
 import { BaseOrderlyKeyPair, OrderlyKeyPair } from "./keyPair";
 import { PublicKey } from "@solana/web3.js";
 import { AbiCoder, keccak256 } from "ethers";
+import { AdditionalInfoRepository } from "./additionalInfoRepository";
+import { LocalStorageRepository } from "./repository";
 
 export interface AccountState {
   status: AccountStatusEnum;
@@ -59,6 +61,7 @@ export interface AccountState {
  */
 export class Account {
   static instanceName = "account";
+  static additionalInfoRepositoryName = "walletAdditionalInfo";
   // private walletClient?: WalletClient;
   private _singer?: Signer;
   // private _state$ = new BehaviorSubject<AccountState>({
@@ -73,6 +76,8 @@ export class Account {
   private walletAdapterManager: WalletAdapterManager;
 
   assetsManager: Assets;
+
+  private additionalInfoRepository: AdditionalInfoRepository;
 
   private _state: AccountState = {
     status: AccountStatusEnum.NotConnected,
@@ -108,6 +113,10 @@ export class Account {
 
     this.assetsManager = new Assets(configStore, this.contractManger, this);
     this.walletAdapterManager = new WalletAdapterManager(walletAdapters);
+
+    this.additionalInfoRepository = new AdditionalInfoRepository(
+      new LocalStorageRepository(Account.additionalInfoRepositoryName)
+    );
 
     this._bindEvents();
   }
@@ -146,6 +155,8 @@ export class Account {
     wallet.chain.id = this.parseChainId(wallet?.chain?.id);
 
     this.keyStore.setAddress(address);
+
+    this.saveAdditionalInfo(address, wallet);
 
     const nextState: AccountState = {
       ...this.stateValue,
@@ -196,6 +207,21 @@ export class Account {
     this._ee.emit(EVENT_NAMES.validateEnd, finallyState);
 
     return finallyState;
+  }
+  private saveAdditionalInfo(
+    address: string,
+    wallet: {
+      [key: string]: any;
+      additionalInfo?: Record<string, any>;
+    }
+  ) {
+    if (typeof wallet.additionalInfo !== "undefined") {
+      if (typeof wallet.additionalInfo !== "object") {
+        console.warn("`wallet.additionalInfo` is not an object");
+      } else {
+        this.additionalInfoRepository.save(address, wallet.additionalInfo);
+      }
+    }
   }
 
   get stateValue(): AccountState {
@@ -606,6 +632,7 @@ export class Account {
 
     if (orderly_key && key_status === "ACTIVE" && expiration > now) {
       this.keyStore.setAddress(address);
+
       this.keyStore.setKey(address, orderlyKey);
       this.keyStore.setAccountId(address, accountId);
 
@@ -701,6 +728,8 @@ export class Account {
     if (!!this.stateValue.address) {
       // this.keyStore.cleanAllKey(this.stateValue.address);
       this.keyStore.removeAddress();
+
+      this.additionalInfoRepository.clear(this.stateValue.address);
     }
 
     const nextState = {
@@ -853,6 +882,13 @@ export class Account {
   //       : "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
   //   };
   // }
+
+  getAdditionalInfo(): Record<string, any> | null {
+    if (!this.stateValue.address) {
+      return null;
+    }
+    return this.additionalInfoRepository.getAll(this.stateValue.address);
+  }
 
   get on() {
     return this._ee.on.bind(this._ee);
