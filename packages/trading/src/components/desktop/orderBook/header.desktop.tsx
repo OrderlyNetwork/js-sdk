7f36986d97cb2d390d348798fbb9209823c6fd6a
Jianan Li <jianan@orderly.network>
2025-04-26 12:46:06 +0800

    fix: fix

--- packages/trading/src/components/desktop/orderBook/header.desktop.tsx
+++ packages/trading/src/components/desktop/orderBook/header.desktop.tsx
@@ -1,11 +1,11 @@
-import { FC, ReactNode, useState } from "react";
+import React, { FC, ReactNode } from "react";
 import {
   Box,
+  CaretDownIcon,
+  CaretUpIcon,
   cn,
   Flex,
   Popover,
-  Select,
-  SelectItem,
 } from "@orderly.network/ui";
 import { useOrderBookContext } from "../../base/orderBook/orderContext";
 import { useTranslation } from "@orderly.network/i18n";
@@ -18,7 +18,7 @@ interface Props {
 export const DesktopHeader: FC<Props> = (props) => {
   const { showTotal } = useOrderBookContext();
   const { t } = useTranslation();
-  const [open, setOpen] = useState(false);
+  const [popoverOpen, setOpen] = React.useState<boolean>(false);
   return (
     <Flex pl={3} justify={"between"} className="oui-py-[6px]">
       <Flex
@@ -26,12 +26,19 @@ export const DesktopHeader: FC<Props> = (props) => {
         className={cn("oui-basis-7/12", showTotal && "oui-basis-1/2")}
       >
         <Box width={"100%"}>
-          <Title id="oui-order-book-header-price">
+          <Title
+            id="oui-order-book-header-price"
+            className="oui-text-base-contrast-36"
+          >
             {`${t("common.price")}(${props.quote})`}
           </Title>
         </Box>
         <Box width={"100%"}>
-          <Title id="oui-order-book-header-qty" justifyEnd>
+          <Title
+            justifyEnd
+            id="oui-order-book-header-qty"
+            className="oui-text-base-contrast-36"
+          >
             {`${t("common.qty")}(${props.base})`}
           </Title>
         </Box>
@@ -42,21 +49,39 @@ export const DesktopHeader: FC<Props> = (props) => {
         className={cn("oui-basis-5/12", showTotal && "oui-basis-1/2")}
       >
         <Box width={"100%"}>
-          {/* <Popover open={open} onOpenChange={setOpen} content={<div>5678</div>}>
-            <Flex> */}
-          <Title id="oui-order-book-header-total-base" justifyEnd>
-            {`${t("common.total")}(${props.base})`}
-          </Title>
-          {/* </Flex>
-          </Popover> */}
-          {/* <Select valueFormatter={() => <div>123</div>} size="xs">
-            <SelectItem value="1">1</SelectItem>
-            <SelectItem value="2">2</SelectItem>
-            <SelectItem value="3">3</SelectItem>
-          </Select> */}
+          <Popover
+            open={popoverOpen}
+            onOpenChange={setOpen}
+            content={<div>123456</div>}
+          >
+            <Flex
+              justify="end"
+              itemAlign="center"
+              className="oui-transition-all oui-cursor-pointer oui-select-none oui-text-base-contrast-36 hover:oui-text-base-contrast"
+            >
+              <Title
+                justifyEnd
+                id="oui-order-book-header-total-base"
+                className=""
+              >
+                {`${t("common.total")}(${props.base})`}
+              </Title>
+              {popoverOpen ? (
+                <CaretUpIcon
+                  color="inherit"
+                  className="oui-text-3xs oui-w-4 oui-h-4"
+                />
+              ) : (
+                <CaretDownIcon
+                  color="inherit"
+                  className="oui-text-3xs oui-w-4 oui-h-4"
+                />
+              )}
+            </Flex>
+          </Popover>
         </Box>
         {showTotal && (
-          <Box width={"100%"}>
+          <Box className="oui-text-base-contrast-36" width={"100%"}>
             <Title id="oui-order-book-header-total-quote" justifyEnd>
               {`${t("common.total")}(${props.quote})`}
             </Title>
@@ -71,13 +96,15 @@ const Title: FC<{
   justifyEnd?: boolean;
   id?: string;
   children: ReactNode;
+  className?: string;
 }> = (props) => {
-  const { children, justifyEnd = false } = props;
+  const { children, className, justifyEnd = false } = props;
   return (
     <Flex
       id={props.id}
       className={cn(
-        "oui-text-base-contrast-36 oui-text-xs oui-items-end",
+        className,
+        "oui-text-xs oui-items-end",
         justifyEnd && "oui-justify-end"
       )}
     >
