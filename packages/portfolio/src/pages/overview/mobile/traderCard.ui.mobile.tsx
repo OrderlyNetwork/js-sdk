5e32e02339ee74c0eb730044375b8966f2961ae7
ruofei <ruofei@orderly.network>
2025-05-21 22:39:29 +0800

    publish internal

--- packages/portfolio/src/pages/overview/mobile/traderCard.ui.mobile.tsx
+++ packages/portfolio/src/pages/overview/mobile/traderCard.ui.mobile.tsx
@@ -80,7 +80,7 @@ export const TraderCardMobile: FC<TraderCardMobileProps> = (props) => {
           className="oui-ml-auto"
           onClick={() =>
             props?.routerAdapter?.onRouteChange({
-              href: "/rewards/affiliate",
+              href: "/rewards/affiliate?tab=trader",
               name: t("tradingRewards.rewards"),
             })
           }
