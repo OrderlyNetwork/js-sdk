25d44fc7ee6ffd0669ac322ae75e355725933db6
tony <tony@woo.network>
2025-06-04 15:01:10 +0800

    fix trading rewards pause i18n

--- packages/i18n/src/locale/module/tradingRewards.ts
+++ packages/i18n/src/locale/module/tradingRewards.ts
@@ -27,7 +27,7 @@ export const tradingRewards = {
 
   "tradingRewards.epochPauseCountdown.title": "Trading rewards will resume in",
   "tradingRewards.eopchStatus.pause":
-    "Trading rewards is paused until further notice. You may continue to claim your past rewards.",
+    "Trading rewards are paused until further notice. You may continue to claim your past rewards.",
   "tradingRewards.eopchStatus.ended":
     "Trading rewards has ended. You may continue to claim your past rewards.",
   "tradingRewards.eopchStatus.linkDescription": "Stay tuned for more updates.",
