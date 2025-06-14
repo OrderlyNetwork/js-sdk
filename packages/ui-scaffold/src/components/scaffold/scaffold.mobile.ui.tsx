d995854dc61bd5d7b32bec0ba10bb4f63ed5e247
Jame Zhan <jame@orderly.network>
2025-06-15 00:13:35 +0800

    update mobile scaffold props

--- packages/ui-scaffold/src/components/scaffold/scaffold.mobile.ui.tsx
+++ packages/ui-scaffold/src/components/scaffold/scaffold.mobile.ui.tsx
@@ -5,17 +5,22 @@ import { BottomNav } from "../bottomNav/bottomNav.ui.mobile";
 import { MainNavMobile } from "../main/mainNav.ui.mobile";
 import { RestrictedInfoWidget } from "../restrictedInfo";
 import { ScaffoldScriptReturn } from "./scaffold.script";
+import { ScaffoldProps } from "./scaffold.widget";
 
-type MobileScaffoldProps = PropsWithChildren<ScaffoldScriptReturn>;
+type MobileScaffoldProps = PropsWithChildren<
+  ScaffoldScriptReturn & ScaffoldProps
+>;
 
 export const MobileScaffold = (props: MobileScaffoldProps) => {
   return (
     <>
       <header className="oui-sticky oui-top-0 oui-z-10 oui-w-full oui-bg-base-10">
-        <MainNavMobile
-          {...props.mainNavProps}
-          routerAdapter={props.routerAdapter}
-        />
+        {props.topBar ?? (
+          <MainNavMobile
+            {...props.mainNavProps}
+            routerAdapter={props.routerAdapter}
+          />
+        )}
       </header>
       <RestrictedInfoWidget className="oui-mx-1 oui-mb-1 oui-bg-base-6" />
 
@@ -32,13 +37,15 @@ export const MobileScaffold = (props: MobileScaffoldProps) => {
       </Box>
 
       <footer className="oui-fixed oui-bottom-0 oui-z-10 oui-w-full oui-bg-base-9 oui-pb-[calc(env(safe-area-inset-bottom))]">
-        <BottomNav
-          mainMenus={props?.bottomNavProps?.mainMenus}
-          current={
-            props?.bottomNavProps?.current || props?.mainNavProps?.initialMenu
-          }
-          onRouteChange={props.routerAdapter?.onRouteChange}
-        />
+        {props.bottomNav ?? (
+          <BottomNav
+            mainMenus={props?.bottomNavProps?.mainMenus}
+            current={
+              props?.bottomNavProps?.current || props?.mainNavProps?.initialMenu
+            }
+            onRouteChange={props.routerAdapter?.onRouteChange}
+          />
+        )}
       </footer>
     </>
   );
