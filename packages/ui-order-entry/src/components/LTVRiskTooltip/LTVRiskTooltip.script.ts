7209a31bb4c8d194c5e8c7381c7f9cdd4c6b65d0
lijianan <574980606@qq.com>
2025-06-30 23:21:53 +0800

    fix: fix LTV value

--- packages/ui-order-entry/src/components/LTVRiskTooltip/LTVRiskTooltip.script.ts
+++ packages/ui-order-entry/src/components/LTVRiskTooltip/LTVRiskTooltip.script.ts
@@ -1,5 +1,15 @@
-import { useQuery } from "@orderly.network/hooks";
+import { useMemo } from "react";
+import {
+  useHoldingStream,
+  useIndexPricesStream,
+  usePositionStream,
+  useQuery,
+} from "@orderly.network/hooks";
+import { account } from "@orderly.network/perp";
+import { useDataTap } from "@orderly.network/react-app";
 import type { API } from "@orderly.network/types";
+
+const { LTV, collateralRatio } = account;
 
 const useConvertThreshold = () => {
   const { data, error, isLoading } = useQuery<API.ConvertThreshold>(
@@ -16,14 +26,61 @@ const useConvertThreshold = () => {
 
 export const useLTVTooltipScript = () => {
   const {
+    usdc,
+    data: holdingList = [],
+    isLoading: isHoldingLoading,
+  } = useHoldingStream();
+
+  const {
     ltv_threshold,
     negative_usdc_threshold,
     isLoading: isThresholdLoading,
   } = useConvertThreshold();
+
+  const { data: testTokenChainsRes } = useQuery<API.Chain[]>(
+    "https://testnet-api.orderly.org/v1/public/token",
+    {},
+  );
+
+  const { data: indexPrices } = useIndexPricesStream();
+
+  const [data] = usePositionStream();
+
+  const aggregated = useDataTap(data.aggregated);
+  const unrealPnL = aggregated?.total_unreal_pnl ?? 0;
+
+  const currentLtv = useMemo(() => {
+    const usdcBalance = usdc?.holding ?? 0;
+    return LTV({
+      usdcBalance: usdcBalance,
+      upnl: unrealPnL,
+      collateralAssets: holdingList.map((item) => {
+        const indexPrice = item.token === "USDC" ? 1 : indexPrices[item.token];
+        const findToken = testTokenChainsRes?.find(
+          (token) => token.token === item.token,
+        );
+        const qty = item?.holding ?? 0;
+        return {
+          qty: qty,
+          indexPrice: indexPrice ?? 1,
+          weight: collateralRatio({
+            baseWeight: findToken?.base_weight ?? 0,
+            discountFactor: findToken?.discount_factor ?? 0,
+            collateralQty: qty,
+            indexPrice: indexPrice ?? 1,
+          }),
+        };
+      }),
+    });
+  }, [usdc?.holding, unrealPnL, holdingList, indexPrices, testTokenChainsRes]);
+
   return {
+    holdingList,
+    isHoldingLoading,
     ltv_threshold,
     negative_usdc_threshold,
     isThresholdLoading,
+    currentLtv,
   };
 };
 
--- packages/ui-order-entry/src/components/LTVRiskTooltip/LTVRiskTooltip.ui.tsx
+++ packages/ui-order-entry/src/components/LTVRiskTooltip/LTVRiskTooltip.ui.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "@orderly.network/i18n";
 import { Button, cn, Divider, Flex, Text } from "@orderly.network/ui";
+import { removeTrailingZeros } from "@orderly.network/utils";
 import type { LTVTooltipScriptReturn } from "./LTVRiskTooltip.script";
 
 const calculateLTVColor = (val: number): string => {
@@ -15,16 +16,15 @@ const calculateLTVColor = (val: number): string => {
   }
 };
 
-const testData = [
-  { symbol: "USDC", amount: "-4000.00" },
-  { symbol: "ETH", amount: "14000.00" },
-];
-
-const currentLtv = 64; // Test data
-
 export const LTVRiskTooltipUI: React.FC<LTVTooltipScriptReturn> = (props) => {
   const { t } = useTranslation();
-  const { ltv_threshold, negative_usdc_threshold, isThresholdLoading } = props;
+  const {
+    ltv_threshold,
+    negative_usdc_threshold,
+    isThresholdLoading,
+    holdingList = [],
+    currentLtv,
+  } = props;
   return (
     <Flex gap={1} className="oui-w-72 oui-max-w-72" direction="column">
       <Flex width={"100%"} justify="between" itemAlign="center">
@@ -35,7 +35,7 @@ export const LTVRiskTooltipUI: React.FC<LTVTooltipScriptReturn> = (props) => {
           Collateral contribution
         </Text>
       </Flex>
-      {testData.map((asset, index) => {
+      {holdingList.map((asset, index) => {
         return (
           <Flex
             key={`item-${index}`}
@@ -44,14 +44,14 @@ export const LTVRiskTooltipUI: React.FC<LTVTooltipScriptReturn> = (props) => {
             itemAlign="center"
           >
             <Text intensity={80} size="xs">
-              {asset.symbol}
+              {asset.token}
             </Text>
             <Text
               size="xs"
               intensity={80}
-              className={cn(Number(asset.amount) < 0 && "oui-text-warning")}
+              className={cn(Number(asset.holding) < 0 && "oui-text-warning")}
             >
-              {asset.amount}
+              {removeTrailingZeros(asset.holding)}
             </Text>
           </Flex>
         );
@@ -70,9 +70,11 @@ export const LTVRiskTooltipUI: React.FC<LTVTooltipScriptReturn> = (props) => {
         </Text>
       </Flex>
       <Text className="oui-py-2" intensity={54} size="xs">
-        If your LTV exceeds 95%, your collateral will be automatically converted
-        to USDC to repay the borrowed amount, incurring a 5% fee. To avoid
-        automatic conversion, you may choose to convert assets to USDC manually.
+        If your LTV exceeds {isThresholdLoading ? "-" : ltv_threshold}% or your
+        USDC balance plus Unsettled PnL falls below{" "}
+        {isThresholdLoading ? "-" : negative_usdc_threshold}, your collateral
+        will be automatically converted with a haircut. To avoid this, you can
+        manually convert assets to USDC.
       </Text>
       <Button fullWidth size={"md"} variant={"outlined"} color={"secondary"}>
         Convert assets to USDC
