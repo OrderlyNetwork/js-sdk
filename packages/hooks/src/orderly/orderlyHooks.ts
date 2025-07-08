fcd41b8d862baca09674500e1e0b0953d735cb4b
lijianan <574980606@qq.com>
2025-07-08 21:11:58 +0800

    update useAssetconvertEvent

--- packages/hooks/src/orderly/orderlyHooks.ts
+++ packages/hooks/src/orderly/orderlyHooks.ts
@@ -45,7 +45,6 @@ export { useCollateral } from "./useCollateral";
 export type { CollateralOutputs } from "./useCollateral";
 export { useMaxQty } from "./useMaxQty";
 export { useMarginRatio, type MarginRatioReturn } from "./useMarginRatio";
-export { useAssetconvertEvent } from "./useAssetconvertStream";
 export { useChains } from "./useChains";
 export { useStorageChain } from "./useStorageChain";
 export type {
