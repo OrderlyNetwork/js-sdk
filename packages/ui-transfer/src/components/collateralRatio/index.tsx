07d7b74321b49c415f3572076478c404f9edb03e
lijianan <574980606@qq.com>
2025-07-09 23:23:55 +0800

    fix: update link

--- packages/ui-transfer/src/components/collateralRatio/index.tsx
+++ packages/ui-transfer/src/components/collateralRatio/index.tsx
@@ -1,3 +1,1 @@
 export { CollateralRatioWidget } from "./collateralRatio.widget";
-export { useCollateralRatioScript } from "./collateralRatio.script";
-export type { CollateralRatioReturns } from "./collateralRatio.script";
