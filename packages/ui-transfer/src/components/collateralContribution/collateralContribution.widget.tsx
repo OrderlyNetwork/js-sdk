1ac276c57e584b32f2f8d28baede67ba4d258d13
Jame Zhan <jame@orderly.network>
2025-07-10 12:09:42 +0800

    update deposit form info

--- packages/ui-transfer/src/components/collateralContribution/collateralContribution.widget.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import { useCollateralContributionScript } from "./collateralContribution.script";
-import { CollateralContributionUI } from "./collateralContribution.ui";
-
-export const CollateralContributionWidget: React.FC<
-  Readonly<{ value: number; precision: number }>
-> = (props) => {
-  return <CollateralContributionUI {...props} />;
-};
