a74665baa2ac5c655a26408d2dc7631ef6c820b8
Jame Zhan <jame@orderly.network>
2025-07-16 10:17:25 +0000

    publish internal patch

--- packages/ui-connector/src/version.ts
+++ packages/ui-connector/src/version.ts
@@ -8,7 +8,7 @@ declare global {
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
   window.__ORDERLY_VERSION__["@orderly.network/ui-connector"] =
-    "2.5.1-internal-20250717.0";
+    "2.5.1-internal-20250717.1";
 }
 
-export default "2.5.1-internal-20250717.0";
+export default "2.5.1-internal-20250717.1";
