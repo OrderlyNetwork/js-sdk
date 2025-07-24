cda5440e3afe7a4df074572489d3b0140ecc1f7c
Jame Zhan <jame@orderly.network>
2025-07-24 23:07:06 +0800

    fix useBalanceSubscription call once

--- packages/hooks/src/orderly/useBalanceSubscription.ts
+++ packages/hooks/src/orderly/useBalanceSubscription.ts
@@ -1,32 +1,30 @@
-import useSWRSubscription from "swr/subscription";
+import { useEffect } from "react";
 import { useAccount } from "../useAccount";
 import { useWS } from "../useWS";
 
-export const useBalanceSubscription = (options?: {
-  onMessage?: (data: any) => void;
+export const useBalanceSubscription = (options: {
+  onMessage: (data: any) => void;
 }) => {
   const ws = useWS();
   const { state } = useAccount();
 
-  return useSWRSubscription(
-    state.accountId ? ["balance", state.accountId] : null,
-    (_, { next }) => {
-      const unsubscribe = ws.privateSubscribe(
-        {
-          id: "balance",
-          event: "subscribe",
-          topic: "balance",
-          ts: Date.now(),
-        },
-        {
-          onMessage: (data: any) => {
-            options?.onMessage?.(data);
-            next(null, data);
-          },
-        },
-      );
+  useEffect(() => {
+    if (!state.accountId) return;
 
-      return () => unsubscribe();
-    },
-  );
+    const unsubscribe = ws.privateSubscribe(
+      {
+        id: "balance",
+        event: "subscribe",
+        topic: "balance",
+        ts: Date.now(),
+      },
+      {
+        onMessage: (data: any) => {
+          options.onMessage(data);
+        },
+      },
+    );
+
+    return () => unsubscribe?.();
+  }, [state.accountId]);
 };
