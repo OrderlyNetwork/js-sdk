6801767990ac26f31c937291872b31723dfe2c4a
lijianan <574980606@qq.com>
2025-07-30 16:22:27 +0800

    perf: reduce re-renders by memoizing Provider

--- packages/trading/src/provider/context.tsx
+++ packages/trading/src/provider/context.tsx
@@ -1,29 +1,37 @@
-import { createContext, PropsWithChildren, useContext } from "react";
+import React, {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useMemo,
+} from "react";
 import { useSymbolsInfo } from "@orderly.network/hooks";
 import { TradingPageState, TradingPageProps } from "../types/types";
 import { getBasicSymbolInfo } from "../utils/utils";
 
 export const TradingPageContext = createContext({} as TradingPageState);
+
 export const useTradingPageContext = () => {
   return useContext(TradingPageContext);
 };
 
-export const TradingPageProvider = (
-  props: PropsWithChildren<TradingPageProps>
-) => {
-  const symbolInfo = useSymbolsInfo()[props.symbol];
-
+export const TradingPageProvider: React.FC<
+  React.PropsWithChildren<TradingPageProps>
+> = (props) => {
+  const { symbol, children } = props;
+  const symbolInfo = useSymbolsInfo()[symbol];
+  const memoizedValue = useMemo<TradingPageState>(() => {
+    const basicSymbol = getBasicSymbolInfo(symbolInfo);
+    return {
+      ...props,
+      symbolInfo: {
+        ...basicSymbol,
+        symbol: symbol,
+      },
+    };
+  }, [props, symbol, symbolInfo]);
   return (
-    <TradingPageContext.Provider
-      value={{
-        ...props,
-        symbolInfo: {
-          ...getBasicSymbolInfo(symbolInfo),
-          symbol: props.symbol,
-        },
-      }}
-    >
-      {props.children}
+    <TradingPageContext.Provider value={memoizedValue}>
+      {children}
     </TradingPageContext.Provider>
   );
 };
