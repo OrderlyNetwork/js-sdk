55944977a80a1e3b659a8c2fa0543f04c30ece6d
lijianan <574980606@qq.com>
2025-07-30 09:41:48 +0800

    rm log

--- packages/ui-orders/src/components/orderList/orderListContext.tsx
+++ packages/ui-orders/src/components/orderList/orderListContext.tsx
@@ -104,8 +104,6 @@ export const OrderListProvider: FC<
     },
   );
 
-  console.log("huhuhhuuuhuhu");
-
   const memoizedValue = useMemo<OrderListContextState>(() => {
     return {
       onCancelOrder,
