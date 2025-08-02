e33c011458ef07641ec6b90f5ee53075f458c24b
Jame Zhan <jame@orderly.network>
2025-08-02 20:28:47 +0800

    chore: rename searchInput.tsx dir name to searchInput

--- packages/markets/src/components/expandMarkets/expandMarkets.ui.tsx
+++ packages/markets/src/components/expandMarkets/expandMarkets.ui.tsx
@@ -3,7 +3,7 @@ import { Box, cn, TabPanel, Tabs } from "@orderly.network/ui";
 import { FavoritesIcon } from "../../icons";
 import { MarketsTabName } from "../../type";
 import { MarketsListWidget } from "../marketsList";
-import { SearchInput } from "../searchInput.tsx";
+import { SearchInput } from "../searchInput";
 import { useFavoritesProps } from "../shared/hooks/useFavoritesExtraProps";
 import { ExpandMarketsScriptReturn } from "./expandMarkets.script";
 
