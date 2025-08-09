b45b4d8ff533b95daf2049bd3ac25f9541817acc
Jame Zhan <jame@orderly.network>
2025-08-10 00:36:19 +0800

    DEX-2222 fix UTC format

--- packages/portfolio/src/pages/overview/shared/useAssetHistory.ts
+++ packages/portfolio/src/pages/overview/shared/useAssetHistory.ts
@@ -1,5 +1,5 @@
 import { useMemo, useState } from "react";
-import { subDays, format, getYear, getMonth, getDate, addDays } from "date-fns";
+import { subDays, format } from "date-fns";
 import {
   useAssetsHistory,
   useCollateral,
@@ -134,11 +134,10 @@ export const useAssetsHistoryData = (
 
   const calculate = (data: API.DailyRow[], totalValue: number | null) => {
     const lastItem = data[data.length - 1];
-    const todayFormattedStr = format(today, "yyyy-MM-dd");
 
     return {
       ...lastItem,
-      date: todayFormattedStr,
+      date: getUTCStr(today),
       perp_volume: 0,
       account_value:
         totalValue !== null ? totalValue : (lastItem?.account_value ?? 0),
@@ -154,9 +153,7 @@ export const useAssetsHistoryData = (
       return data;
     }
 
-    const UTCStr = `${today.getUTCFullYear()}-${`${today.getUTCMonth() + 1}`.padStart(2, "0")}-${`${today.getUTCDate()}`.padStart(2, "0")}`;
-
-    if (data[data.length - 1].date === UTCStr) {
+    if (data[data.length - 1].date === getUTCStr(today)) {
       return data;
     }
 
@@ -251,3 +248,11 @@ export const useAssetsHistoryData = (
 export type useAssetsHistoryDataReturn = ReturnType<
   typeof useAssetsHistoryData
 >;
+
+function getUTCStr(date: Date) {
+  const year = date.getUTCFullYear();
+  const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+  const day = `0${date.getUTCDate()}`.slice(-2);
+
+  return `${year}-${month}-${day}`;
+}
