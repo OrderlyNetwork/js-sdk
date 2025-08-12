73a117746ae2c7880cf63595eeb6177ececfe969
lijianan <574980606@qq.com>
2025-08-12 15:17:08 +0800

    fix coinUnit useLocalStorage

--- packages/trading/src/components/mobile/orderBook/cell.tsx
+++ packages/trading/src/components/mobile/orderBook/cell.tsx
@@ -32,11 +32,10 @@ export const OrderBookCell: FC<OrderBookCellProps> = (props) => {
 
   const { base_dp, quote_dp, base, quote } = symbolInfo;
 
-  const [coinTypeConfig, setCoinTypeConfig]: [string, React.Dispatch<string>] =
-    useLocalStorage(ORDERBOOK_MOBILE_COIN_TYPE_KEY, ["total", base].join("_"));
-
-  const [mode, currency] =
-    (coinTypeConfig?.split("_") as [string, string]) ?? [];
+  const [coinUnit, setCoinUnit] = useLocalStorage<"qty" | "base" | "quote">(
+    ORDERBOOK_MOBILE_COIN_TYPE_KEY,
+    "qty",
+  );
 
   const totalAmount = Number.isNaN(accumulated)
     ? "-"
@@ -66,17 +65,17 @@ export const OrderBookCell: FC<OrderBookCellProps> = (props) => {
         >
           {price}
         </Text.numeral>
-        {mode === "qty" && (
+        {coinUnit === "qty" && (
           <Text.numeral className="oui-text-base-contrast-80" dp={base_dp}>
             {Number.isNaN(quantity) ? "-" : quantity}
           </Text.numeral>
         )}
-        {mode === "total" && currency === base && (
+        {coinUnit === "base" && (
           <Text.numeral className="oui-text-base-contrast-80" dp={base_dp}>
             {accumulated}
           </Text.numeral>
         )}
-        {mode === "total" && currency === quote && (
+        {coinUnit === "quote" && (
           <Text.numeral className="oui-text-base-contrast-80" dp={0}>
             {totalAmount}
           </Text.numeral>
