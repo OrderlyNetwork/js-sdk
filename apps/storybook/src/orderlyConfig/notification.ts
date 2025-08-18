b8cff2cedb95a3cf7668fd5325149d38c0fce9cc
lijianan <574980606@qq.com>
2025-08-18 14:34:16 +0800

    update media url

--- apps/storybook/src/orderlyConfig/notification.ts
+++ apps/storybook/src/orderlyConfig/notification.ts
@@ -1,8 +1,7 @@
 import type { OrderlyAppProviderProps } from "@orderly.network/react-app";
-import mediaSrc from "./audio/coin.mp3";
 
 export const notification: OrderlyAppProviderProps["notification"] = {
   orderFilled: {
-    media: mediaSrc,
+    media: "https://oss.orderly.network/static/sdk/coin.mp3",
   },
 };
