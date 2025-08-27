2775471692b76321acbd86703efab070726ecb59
ruofei <ruofei@orderly.network>
2025-08-27 15:49:39 +0800

    fix: vault order status color

--- packages/vaults/src/utils/getOperationStatusColor.ts
+++ packages/vaults/src/utils/getOperationStatusColor.ts
@@ -2,7 +2,7 @@ export const getOperationStatusColor = (status: string) => {
   if (status === "completed") {
     return "success";
   }
-  if (status === "rejected") {
+  if (status === "rejected" || status === "failed") {
     return "danger";
   }
   return "primary";
