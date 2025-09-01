24bbbd41acac57ffd62c5aefa10fd1afbecdb758
lijianan <574980606@qq.com>
2025-09-01 19:51:02 +0800

    update dynamic import for chunk

--- packages/ui-scaffold/src/components/scanQRCode/scanQRCode.widget.tsx
+++ packages/ui-scaffold/src/components/scanQRCode/scanQRCode.widget.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { useScanQRCodeScript } from "./scanQRCode.script";
 import { ScanQRCode } from "./scanQRCode.ui";
 
-export const ScanQRCodeWidget = () => {
+export const ScanQRCodeWidget: React.FC = () => {
   const state = useScanQRCodeScript();
   return <ScanQRCode {...state} />;
 };
