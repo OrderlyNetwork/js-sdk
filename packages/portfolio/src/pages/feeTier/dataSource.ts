71a8639051d8182bd4d87f5150cfe166f5407e83
lijianan <574980606@qq.com>
2025-09-03 15:14:03 +0800

    fix wrong 0

--- packages/portfolio/src/pages/feeTier/dataSource.ts
+++ packages/portfolio/src/pages/feeTier/dataSource.ts
@@ -6,13 +6,13 @@ export const dataSource: FeeDataType[] = [
     volume_min: 0,
     volume_max: 500000,
     maker_fee: "0.025%",
-    taker_fee: "0.040%",
+    taker_fee: "0.04%",
   },
   {
     tier: 2,
     volume_min: 500000,
     volume_max: 1000000,
-    maker_fee: "0.020%",
+    maker_fee: "0.02%",
     taker_fee: "0.035%",
   },
   {
@@ -20,7 +20,7 @@ export const dataSource: FeeDataType[] = [
     volume_min: 1000000,
     volume_max: 5000000,
     maker_fee: "0.018%",
-    taker_fee: "0.030%",
+    taker_fee: "0.03%",
   },
   {
     tier: 4,
@@ -50,7 +50,7 @@ export const dataSource: FeeDataType[] = [
     volume_min: 100000000,
     volume_max: undefined,
     or: "/",
-    maker_fee: "0.000%",
+    maker_fee: "0%",
     taker_fee: "0.015%",
   },
 ];
