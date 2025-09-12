f64a6a7ddaad6f346c137a880b9a6e324aa4449c
lijianan <574980606@qq.com>
2025-09-13 06:37:43 +0800

    update UI

--- apps/storybook/src/playground/pages/portfolio/fee/page.tsx
+++ apps/storybook/src/playground/pages/portfolio/fee/page.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useMemo } from "react";
+import { useTrack } from "@orderly.network/hooks";
 import { useTranslation } from "@orderly.network/i18n";
 import { FeeTierModule } from "@orderly.network/portfolio";
 import { ARBITRUM_MAINNET_CHAINID } from "@orderly.network/types";
-import { Flex, Text, useScreen } from "@orderly.network/ui";
-import type { Column } from "@orderly.network/ui";
+import { Button, Flex, Text, useScreen } from "@orderly.network/ui";
+import type { ButtonProps, Column } from "@orderly.network/ui";
 
 const useWooStaking = (inputs: { decimals: number; chainId: number }) => {
   return {
@@ -13,12 +14,64 @@ const useWooStaking = (inputs: { decimals: number; chainId: number }) => {
   };
 };
 
+const TopRightIcon: React.FC<React.SVGAttributes<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      width={12}
+      height={13}
+      viewBox="0 0 12 13"
+      fill="currentColor"
+      focusable={false}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M8.49512 10.9954C9.59962 10.9954 10.4951 10.0999 10.4951 8.99536V3.99536C10.4951 2.89086 9.59962 1.99536 8.49512 1.99536H3.49512C2.39062 1.99536 1.49512 2.89086 1.49512 3.99536V8.99536C1.49512 10.0999 2.39062 10.9954 3.49512 10.9954H8.49512ZM4.49512 8.49536C4.36712 8.49536 4.23362 8.45236 4.13562 8.35486C3.94062 8.15936 3.94062 7.83136 4.13562 7.63586L6.13562 5.63586L4.99512 4.49536H7.99512V7.49536L6.85461 6.35486L4.85461 8.35486C4.75711 8.45236 4.62312 8.49536 4.49512 8.49536Z"
+        fill="#fff"
+        fillOpacity={0.8}
+      />
+    </svg>
+  );
+};
+
+const TradingBtn: React.FC<ButtonProps> = (props) => {
+  const { t } = useTranslation();
+  return (
+    <Button {...props}>
+      <Flex gap={1}>
+        <Text>{t("common.trading")}</Text>
+        <TopRightIcon />
+      </Flex>
+    </Button>
+  );
+};
+
+const StakeBtn: React.FC<ButtonProps> = (props) => {
+  const { t } = useTranslation();
+  return (
+    <Button
+      onClick={() => window.open("https://woofi.com/swap/stake", "_blank")}
+      {...props}
+    >
+      <Flex gap={1}>
+        <Text>{t("tradingRewards.stake")} WOO</Text>
+        <TopRightIcon />
+      </Flex>
+    </Button>
+  );
+};
+
 const FeeTierPage: React.FC = () => {
   const { t } = useTranslation();
   const { isMobile } = useScreen();
 
+  const { formattedData, isLoading } = useWooStaking({
+    decimals: 18,
+    chainId: ARBITRUM_MAINNET_CHAINID,
+  });
+
   const customDataSource = useMemo(() => {
-    return [
+    const baseData: any[] = [
       {
         tier: 1,
         or: "/",
@@ -62,7 +115,15 @@ const FeeTierPage: React.FC = () => {
         description: `above 2209.7K WOO`,
       },
     ];
-  }, [t]);
+    if (!isMobile) {
+      baseData.push({
+        tier: null,
+        or: "",
+        staking_level: <StakeBtn size={"sm"} />,
+      });
+    }
+    return baseData;
+  }, [t, isMobile]);
 
   const dataAdapter = useCallback(
     (columns: Column[], dataSource: any[]) => {
@@ -96,6 +157,21 @@ const FeeTierPage: React.FC = () => {
         ...columns.slice(2),
       ];
 
+      if (dataSource.length === 7 && !isMobile) {
+        dataSource.push({
+          tier: "",
+          maker_fee: "",
+          taker_fee: "",
+          volume_min: null,
+          volume_max: null,
+          volume_node: <TradingBtn size={"sm"} />,
+          or: "",
+          staking: null,
+          staking_min: null,
+          staking_max: null,
+        });
+      }
+
       return {
         columns: cols,
         dataSource: dataSource?.map((item, index) => ({
@@ -104,13 +180,8 @@ const FeeTierPage: React.FC = () => {
         })),
       };
     },
-    [t],
+    [t, customDataSource, isMobile],
   );
-
-  const { formattedData, isLoading } = useWooStaking({
-    decimals: 18,
-    chainId: ARBITRUM_MAINNET_CHAINID,
-  });
 
   const headerDataAdapter = useCallback(
     (original: any[]) => {
@@ -128,35 +199,44 @@ const FeeTierPage: React.FC = () => {
         ...original.slice(2),
       ];
     },
-    [t, isLoading, formattedData],
+    [t, isMobile, isLoading, formattedData],
   );
 
   return (
-    <FeeTierModule.FeeTierPage
-      dataAdapter={dataAdapter}
-      headerDataAdapter={headerDataAdapter}
-      onRow={() => {
-        return {
-          normal: {
-            className: "oui-h-[54px]",
-          },
-          active: {
-            className:
-              "oui-pointer-events-none oui-h-[54px] oui-text-[rgba(0,0,0,0.88)] oui-gradient-primary",
-          },
-        };
-      }}
-      onCell={() => {
-        return {
-          normal: {
-            //
-          },
-          active: {
-            //
-          },
-        };
-      }}
-    />
+    <>
+      <FeeTierModule.FeeTierPage
+        key={isMobile ? "mobile" : "desktop"}
+        dataAdapter={dataAdapter}
+        headerDataAdapter={headerDataAdapter}
+        onRow={() => {
+          return {
+            normal: {
+              className: "oui-h-[54px]",
+            },
+            active: {
+              className:
+                "oui-pointer-events-none oui-h-[54px] oui-text-[rgba(0,0,0,0.88)] oui-gradient-primary",
+            },
+          };
+        }}
+        onCell={() => {
+          return {
+            normal: {
+              //
+            },
+            active: {
+              //
+            },
+          };
+        }}
+      />
+      {isMobile && (
+        <Flex m={4} gapX={3} itemAlign={"center"} justify={"between"}>
+          <TradingBtn size="md" fullWidth />
+          <StakeBtn size="md" fullWidth />
+        </Flex>
+      )}
+    </>
   );
 };
 
