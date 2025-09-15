021f21621cdcc44d666346cc98b459871413eb75
mark <mark.pan@woo.network>
2025-09-15 15:35:52 +0800

    DEX-2285 [SDK] skip the confirmation for cancelling pending / TP/SL order (mweb)

--- packages/ui-orders/src/components/orderList/mobile/cancelBtn/cancelBtn.ui.tsx
+++ packages/ui-orders/src/components/orderList/mobile/cancelBtn/cancelBtn.ui.tsx
@@ -1,54 +1,21 @@
 import { FC } from "react";
+import { useTranslation } from "@orderly.network/i18n";
 import { Button, SimpleDialog, Text } from "@orderly.network/ui";
 import { CancelBtnState } from "./cancelBtn.script";
-import { useTranslation } from "@orderly.network/i18n";
 
 export const CancelBtn: FC<CancelBtnState> = (props) => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <Button
-        variant="outlined"
-        fullWidth
-        color="secondary"
-        size="sm"
-        className="oui-border-base-contrast-36"
-        onClick={(e) => props.setOpen(true)}
-      >
-        {t("common.cancel")}
-      </Button>
-      {props.open && (
-        <SimpleDialog
-          size="xs"
-          open={props.open}
-          onOpenChange={props.setOpen}
-          title={t("orders.cancelOrder")}
-          actions={{
-            primary: {
-              label: t("common.confirm"),
-              loading: props.isLoading,
-              fullWidth: true,
-              size: "md",
-              onClick: (e) => {
-                props.onCancel(e);
-              },
-            },
-            secondary: {
-              label: t("common.cancel"),
-              fullWidth: true,
-              size: "md",
-              onClick: () => {
-                props.onClose();
-              },
-            },
-          }}
-        >
-          <Text size="2xs" intensity={54}>
-            {t("orders.cancelOrder.description")}
-          </Text>
-        </SimpleDialog>
-      )}
-    </>
+    <Button
+      variant="outlined"
+      fullWidth
+      color="secondary"
+      size="sm"
+      className="oui-border-base-contrast-36"
+      onClick={(e) => props.onCancel(e)}
+    >
+      {t("common.cancel")}
+    </Button>
   );
 };
