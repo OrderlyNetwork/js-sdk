2515988ef19aaf136c628fa919c2af31ffab9e21
Han <han@orderly.network>
2025-11-20 16:06:17 +0800

    Add starchild mainnet tg bots

--- apps/storybook/src/orderlyConfig/starChildConfig.ts
+++ apps/storybook/src/orderlyConfig/starChildConfig.ts
@@ -1,15 +1,30 @@
 import type { ConfigStore } from "@orderly.network/core";
 import type { OrderlyAppProviderProps } from "@orderly.network/react-app";
 
+enum TelegramBotId {
+  Woofi = "7989045229", // for woofi.com
+  WoofiPro = "8354814497", // for pro.woofi.com
+  Test = "7573076643", // for testnet
+}
+
 export const getStarChildConfig = (
   configStore: ConfigStore,
 ): OrderlyAppProviderProps["starChildConfig"] => {
   const appEnv = configStore.get("env") as string;
   const isProduction = appEnv === "prod";
 
-  return {
-    enable: true,
-    env: isProduction ? "mainnet" : "testnet",
-    telegram_bot_id: "7573076643",
-  };
+  const enable = true;
+  if (isProduction) {
+    return {
+      enable,
+      env: "mainnet",
+      telegram_bot_id: TelegramBotId.WoofiPro,
+    };
+  } else {
+    return {
+      enable,
+      env: "testnet",
+      telegram_bot_id: TelegramBotId.Test,
+    };
+  }
 };
