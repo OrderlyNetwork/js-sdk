29cfbf466dd83fbcef7b1c4d28d32178b4c57fac
Han <han@orderly.network>
2025-11-26 13:36:46 +0800

    Sync pre-release version

--- packages/ui-starchild-widget/src/version.ts
+++ packages/ui-starchild-widget/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@orderly.network/ui-starchild-widget"] = "2.8.2";
+  window.__ORDERLY_VERSION__["@orderly.network/ui-starchild-widget"] = "2.8.4";
 }
 
-export default "2.8.2";
+export default "2.8.4";
