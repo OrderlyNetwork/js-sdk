0e7265280e9adb23d281f52f53131f78a817d7af
Arnold Zhang <arnold@orderly.network>
2026-02-26 15:34:23 +0800

    fix: adjust Markets panel width for mweb and web (DEX-2788)

--- packages/markets/src/components/symbolInfoBarFull/symbolInfoBarFull.ui.tsx
+++ packages/markets/src/components/symbolInfoBarFull/symbolInfoBarFull.ui.tsx
@@ -111,7 +111,7 @@ export const SymbolInfoBarFull: React.FC<SymbolInfoBarFullProps> = (props) => {
         className="oui-shrink-0 oui-gap-y-[2px]"
       >
         <LazyDropDownMarketsWidget
-          contentClassName="oui-w-[429px] oui-h-[496px]"
+          contentClassName="oui-w-[580px] oui-h-[496px]"
           symbol={props.symbol}
           onSymbolChange={props.onSymbolChange}
         >
