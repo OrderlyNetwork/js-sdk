00cedcf99433912d240f22cc10b3cf28b0c81e8f
mark <mark.pan@woo.network>
2026-03-03 18:30:41 +0800

    DEX-2808 [SDK] Add bulk close options by position profitability

--- packages/ui-positions/src/components/closeAllPositions/index.ts
+++ packages/ui-positions/src/components/closeAllPositions/index.ts
@@ -3,6 +3,7 @@ export {
   type CloseAllPositionsProps,
 } from "./closeAllPositions.ui";
 export {
+  CloseType,
   useCloseAllPositionsScript,
   type CloseAllPositionsState,
 } from "./closeAllPositions.script";
