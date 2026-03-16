a4794d79410b8d6dbbcaa1e6b7291b011dab31c6
mark <mark.pan@woo.network>
2026-03-16 23:23:12 +0800

    DEX-2856 [SDK] ISO short position effective leverage calculation error

--- packages/hooks/src/orderly/calculator/positions.ts
+++ packages/hooks/src/orderly/calculator/positions.ts
@@ -267,18 +267,35 @@ class PositionCalculator extends BaseCalculator<API.PositionInfo> {
                 .toNumber()
             : crossMarginCollateral.toNumber();
 
-        const est_liq_price = positions.liqPrice({
-          symbol: item.symbol,
-          markPrice: item.mark_price,
-          totalCollateral: totalCollateral,
-          positionQty: item.position_qty,
-          positions: rows,
-          MMR: item.mmr,
-          baseMMR: info?.["base_mmr"],
-          baseIMR: info?.["base_imr"],
-          IMRFactor: accountInfo.imr_factor[item.symbol] as number,
-          costPosition: item.cost_position,
-        });
+        let est_liq_price;
+        if (item.margin_mode === MarginMode.ISOLATED) {
+          est_liq_price = positions.liquidationPriceIsolated({
+            isolatedPositionMargin: item.margin ?? 0,
+            costPosition: item.cost_position ?? 0,
+            positionQty: item.position_qty ?? 0,
+            sumUnitaryFunding:
+              fundingRates?.[item.symbol]?.["sum_unitary_funding"] ?? 0,
+            lastSumUnitaryFunding: item.last_sum_unitary_funding ?? 0,
+            baseMMR: info?.["base_mmr"] ?? 0,
+            baseIMR: info?.["base_imr"] ?? 0,
+            IMRFactor: accountInfo.imr_factor[item.symbol] as number,
+            referencePrice: item.mark_price,
+            leverage: item.leverage ?? 0,
+          });
+        } else {
+          est_liq_price = positions.liqPrice({
+            symbol: item.symbol,
+            markPrice: item.mark_price,
+            totalCollateral: totalCollateral,
+            positionQty: item.position_qty,
+            positions: rows,
+            MMR: item.mmr,
+            baseMMR: info?.["base_mmr"],
+            baseIMR: info?.["base_imr"],
+            IMRFactor: accountInfo.imr_factor[item.symbol] as number,
+            costPosition: item.cost_position,
+          });
+        }
         return {
           ...item,
           est_liq_price,
