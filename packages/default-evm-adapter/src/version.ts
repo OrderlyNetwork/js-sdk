88081dac32bf4b61278397b22ecea3a53f7893ba
Gitlab CI <leo@orderly.network>
2026-07-10 10:02:12 +0000

    publish alpha

--- packages/default-evm-adapter/src/version.ts
+++ packages/default-evm-adapter/src/version.ts
@@ -7,7 +7,8 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@orderly.network/default-evm-adapter"] = "3.1.3";
+  window.__ORDERLY_VERSION__["@orderly.network/default-evm-adapter"] =
+    "3.1.4-alpha.0";
 }
 
-export default "3.1.3";
+export default "3.1.4-alpha.0";
--- packages/hooks/src/version.ts
+++ packages/hooks/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@orderly.network/hooks"] = "3.1.3";
+  window.__ORDERLY_VERSION__["@orderly.network/hooks"] = "3.1.4-alpha.0";
 }
 
-export default "3.1.3";
+export default "3.1.4-alpha.0";
