1cee985f6962378a2741eaf4b31245d69484487b
Gitlab CI <leo@orderly.network>
2026-07-24 04:29:37 +0000

    publish alpha

--- packages/cli/src/version.ts
+++ packages/cli/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@orderly.network/devkit"] = "1.1.7-alpha.1";
+  window.__ORDERLY_VERSION__["@orderly.network/devkit"] = "1.1.7-alpha.2";
 }
 
-export default "1.1.7-alpha.1";
+export default "1.1.7-alpha.2";
--- packages/perp/src/version.ts
+++ packages/perp/src/version.ts
@@ -7,7 +7,7 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@orderly.network/perp"] = "5.1.7-alpha.1";
+  window.__ORDERLY_VERSION__["@orderly.network/perp"] = "5.1.7-alpha.2";
 }
 
-export default "5.1.7-alpha.1";
+export default "5.1.7-alpha.2";
