ca8d935e49b844191e3bdf06fcd6c3199273cc80
Gitlab CI <jame@orderly.network>
2026-08-31 02:04:05 +0000

    Release v3.1.12-alpha.0

--- packages/trading-leaderboard/src/version.ts
+++ packages/trading-leaderboard/src/version.ts
@@ -7,7 +7,8 @@ declare global {
 }
 if (typeof window !== "undefined") {
   window.__ORDERLY_VERSION__ = window.__ORDERLY_VERSION__ || {};
-  window.__ORDERLY_VERSION__["@orderly.network/trading-leaderboard"] = "3.1.11";
+  window.__ORDERLY_VERSION__["@orderly.network/trading-leaderboard"] =
+    "3.1.12-alpha.0";
 }
 
-export default "3.1.11";
+export default "3.1.12-alpha.0";
